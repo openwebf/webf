14af1013425f36328189a5684c68b27e59684e10
jiangzhou <jiangzhoubai@gmail.com>
2022-08-21 18:33:17 +0800

    feat: remove & append link element

--- integration_tests/specs/dom/elements/link.ts
+++ integration_tests/specs/dom/elements/link.ts
@@ -17,7 +17,7 @@ describe('Link Element', () => {
     BODY.appendChild(div);
   });
 
-  fit('should work with local css', async (done) => {
+  it('should work with local css', async (done) => {
     let link = document.createElement('link');
     link.setAttribute('href', 'assets:assets/bad.css');
     link.setAttribute('rel', 'stylesheet');
@@ -34,5 +34,47 @@ describe('Link Element', () => {
     BODY.appendChild(p);
   });
 
-  
+  it('insert style sheet', async (done) => {
+    let link1 = document.createElement('link');
+    link1.setAttribute('href', 'assets:assets/bad.css');
+    link1.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(link1);
+
+    let link2 = document.createElement('link');
+    link2.setAttribute('href', 'assets:assets/good.css');
+    link2.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(link2);
+
+    link2.addEventListener('load', async () => {
+      await snapshot();
+      done();
+    });
+
+    let p = document.createElement('p');
+    p.appendChild(document.createTextNode('This text should be green on a red background'));
+    BODY.appendChild(p);
+  });
+
+  it('remove style sheet', async (done) => {
+    let link1 = document.createElement('link');
+    link1.setAttribute('href', 'assets:assets/bad.css');
+    link1.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(link1);
+
+    let link2 = document.createElement('link');
+    link2.setAttribute('href', 'assets:assets/good.css');
+    link2.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(link2);
+    
+    link1.addEventListener('load', async () => {
+      document.head.removeChild(link1);
+      await sleep(0.5);
+      await snapshot();
+      done();
+    });
+
+    let p = document.createElement('p');
+    p.appendChild(document.createTextNode('remove: This text should be green on a red background'));
+    BODY.appendChild(p);
+  });
 });
--- webf/lib/src/css/parser/parser.dart
+++ webf/lib/src/css/parser/parser.dart
@@ -128,10 +128,8 @@ class CSSParser {
 
   List<CSSRule> parseRules({int startPosition = 0}) {
     var rules = <CSSRule>[];
-    int position = startPosition;
     while (!_maybeEat(TokenKind.END_OF_FILE)) {
       final rule = processRule();
-      rule?.position = position++;
       if (rule != null) {
         rules.add(rule);
       } else {
