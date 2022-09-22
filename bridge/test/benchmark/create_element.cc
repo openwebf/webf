db603f1024db8da05a9cee5905e6fd00078ccccd
andycall <dongtiangche@outlook.com>
2022-09-23 01:08:41 +0800

    feat: add node operation benchmark

--- bridge/test/benchmark/create_element.cc
+++ bridge/test/benchmark/create_element.cc
@@ -4,31 +4,75 @@
  */
 
 #include <benchmark/benchmark.h>
-#include "page.h"
 #include "webf_test_env.h"
+
+using namespace webf;
 
 auto bridge = TEST_init();
 
 static void CreateRawJavaScriptObjects(benchmark::State& state) {
-  auto& context = bridge->GetExecutingContext();
-  std::string code = "var a = {}";
+  auto context = bridge->GetExecutingContext();
+  uint8_t bytes[] = {
+      1,2,2,97,12,97,97,97,46,106,115,14,0,6,0,160,1,0,1,0,1,0,0,20,1,162,1,0,0,0,63,210,0,0,0,0,62,210,0,0,0,0,11,57,210,0,0,0,195,40,166,3,1,2,31,33
+  };
   // Perform setup here
   for (auto _ : state) {
-    context->EvaluateJavaScript(code.c_str(), code.size(), "internal://", 0);
+    context->EvaluateByteCode(bytes, sizeof(bytes));
   }
 }
 
 static void CreateDivElement(benchmark::State& state) {
-  auto& context = bridge->GetExecutingContext();
-  std::string code = "var a = document.kCreateElement('div');";
+  auto context = bridge->GetExecutingContext();
+  std::string code = R"(
+(() => {
+let container = document.createElement('div');
+for(let i = 0; i < 1000; i ++) {
+    let child = document.createElement('div');
+    for(let j = 0; j < 10; j ++) {
+        let span = document.createElement('span');
+        let text = document.createElement('helloworld');
+        span.appendChild(text);
+        child.appendChild(span);
+    }
+    container.appendChild(child);
+}
+})();
+)";
+  // Perform setup here
+  for (auto _ : state) {
+        context->EvaluateJavaScript(code.c_str(), code.size(), "internal://", 0);
+  }
+}
+
+static void InsertElement(benchmark::State& state) {
+  auto context = bridge->GetExecutingContext();
+  std::string code = R"(
+(() => {
+let container = document.createElement('div');
+let child = document.createElement('div');
+let span = document.createElement('span');
+let text = document.createElement('helloworld');
+span.appendChild(text);
+container.appendChild(child);
+
+for(let i = 0; i < 1000; i ++) {
+    let span = document.createElement('span');
+    let text = document.createElement('helloworld');
+    span.appendChild(text);
+    child.insertBefore(span, child.firstChild);
+}
+})();
+)";
   // Perform setup here
   for (auto _ : state) {
     context->EvaluateJavaScript(code.c_str(), code.size(), "internal://", 0);
   }
 }
 
+
 BENCHMARK(CreateRawJavaScriptObjects)->Threads(1);
 BENCHMARK(CreateDivElement)->Threads(1);
+BENCHMARK(InsertElement)->Threads(1);
 
 // Run the benchmark
 BENCHMARK_MAIN();
