737b5902216eb099d95c85e911ab13678b29f2f5
andycall <dongtiangche@outlook.com>
2022-09-26 12:23:10 +0800

    chore: remove deprecated native apis.

--- webf/android/src/main/java/com/openwebf/webf/WebF.java
+++ webf/android/src/main/java/com/openwebf/webf/WebF.java
@@ -12,8 +12,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
 public class WebF {
-
-  private String url;
   private String dynamicLibraryPath;
   private FlutterEngine flutterEngine;
 
@@ -36,13 +34,6 @@ public class WebF {
   public void registerMethodCallHandler(MethodChannel.MethodCallHandler handler) {
     this.handler = handler;
   }
-  /**
-   * Load url.
-   * @param url
-   */
-  public void loadUrl(String url) {
-    this.url = url;
-  }
 
   /**
    * Set the dynamic library path.
@@ -51,46 +42,8 @@ public class WebF {
   public void setDynamicLibraryPath(String value) {
     this.dynamicLibraryPath = value;
   }
-
-  public String getUrl() {
-    return url;
-  }
-
   public String getDynamicLibraryPath() {
     return dynamicLibraryPath != null ? dynamicLibraryPath : "";
-  }
-
-  public void _handleMethodCall(MethodCall call, MethodChannel.Result result) {
-    if (this.handler != null) {
-      this.handler.onMethodCall(call, result);
-    } else {
-      result.error("No handler found.", null, null);
-    }
-  }
-
-  public void invokeMethod(final String method, final Object arguments) {
-    new Handler(Looper.getMainLooper()).post(new Runnable() {
-      @Override
-      public void run() {
-        if (flutterEngine != null) {
-          PluginRegistry pluginRegistry = flutterEngine.getPlugins();
-          WebFPlugin webFPlugin = (WebFPlugin) pluginRegistry.get(WebFPlugin.class);
-          if (webFPlugin != null && webFPlugin.channel != null) {
-            webFPlugin.channel.invokeMethod(method, arguments);
-          }
-        }
-      }
-    });
-  }
-
-  public void reload() {
-    if (flutterEngine != null) {
-      PluginRegistry pluginRegistry = flutterEngine.getPlugins();
-      WebFPlugin webFPlugin = (WebFPlugin) pluginRegistry.get(WebFPlugin.class);
-      if (webFPlugin != null) {
-        webFPlugin.reload();
-      }
-    }
   }
 
   public void destroy() {
--- webf/macos/Classes/WebFPlugin.h
+++ webf/macos/Classes/WebFPlugin.h
@@ -1,7 +1,5 @@
 #import <FlutterMacOS/FlutterMacOS.h>
 
-#define NAME_METHOD_SPLIT @"!!"
-
 @interface WebFPlugin : NSObject<FlutterPlugin>
 
 @property NSObject<FlutterPluginRegistrar> *registrar;
