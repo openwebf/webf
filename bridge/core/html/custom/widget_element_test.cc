2da12866ddcab279b8737bc021de9612129d8f67
andycall <dongtiangche@outlook.com>
2022-09-28 20:07:22 +0800

    fix: fix set event handler property to custom element.

---
+++ bridge/core/html/custom/widget_element_test.cc
@@ -0,0 +1,31 @@
+/*
+* Copyright (C) 2022-present The WebF authors. All rights reserved.
+*/
+
+#include "widget_element.h"
+#include "gtest/gtest.h"
+#include "webf_test_env.h"
+
+using namespace webf;
+
+TEST(WidgetElement, setPropertyEventHandler) {
+ bool static errorCalled = false;
+ bool static logCalled = false;
+ webf::WebFPage::consoleMessageHandler = [](void* ctx, const std::string& message, int logLevel) {
+   EXPECT_STREQ(message.c_str(), "1111");
+   logCalled = true;
+ };
+ auto bridge = TEST_init([](int32_t contextId, const char* errmsg) {
+   WEBF_LOG(VERBOSE) << errmsg;
+   errorCalled = true;
+ });
+ auto context = bridge->GetExecutingContext();
+ const char* code =
+     "let checkbox = document.createElement('flutter-checkbox'); "
+     "function f(){ console.log(1111); }; "
+     "checkbox.onclick = f; "
+     "checkbox.dispatchEvent(new Event('click'));";
+ bridge->evaluateScript(code, strlen(code), "vm://", 0);
+
+ EXPECT_EQ(errorCalled, false);
+}
