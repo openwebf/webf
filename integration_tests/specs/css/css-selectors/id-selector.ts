897ef3f5f85a00cee34fa8f3a19d92d2eb990d5b
jiangzhou <jiangzhoubai@gmail.com>
2022-10-08 17:23:44 +0800

    fix: code review for word space

--- integration_tests/specs/css/css-selectors/id-selector.ts
+++ integration_tests/specs/css/css-selectors/id-selector.ts
@@ -1,54 +1,54 @@
 describe("css id selector", () => {
   it("001", async () => {
     const style = <style>{`#div1 { color: green; }`}</style>;
-    const div = <div id="div1"> 001 green Filler Text</div>;
+    const div = <div id="div1">001 green Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
 
   it("002", async () => {
     const style = <style>{`# div1 { color: green; }`}</style>;
-    const div = <div id="div1"> 002 black Filler Text</div>;
+    const div = <div id="div1">002 black Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
 
   it("003", async () => {
     const style = <style>{`div { color: red; } #-div1 { color: green; }`}</style>;
-    const div = <div id="-div1"> 003 green Filler Text</div>;
+    const div = <div id="-div1">003 green Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
 
   xit("004", async () => {
     const style = <style>{`#1digit { color: red; }`}</style>;
-    const div = <div id="1digit"> 004 black Filler Text</div>;
+    const div = <div id="1digit">004 black Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
 
   it("005", async () => {
     const style = <style>{`div[id=div1] { color: red; } div#div1 { color: green; }`}</style>;
-    const div = <div id="div1"> 005 green Filler Text</div>;
+    const div = <div id="div1">005 green Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
 
   it("006", async () => {
     const style = <style>{`div[id=div1] { color: red; } div#div1 { color: green; }`}</style>;
-    const div = <div id="div1"> 006 green Filler Text</div>;
+    const div = <div id="div1">006 green Filler Text</div>;
     
-       document.head.appendChild(style);
+    document.head.appendChild(style);
     document.body.appendChild(div);
     await snapshot();
   });
