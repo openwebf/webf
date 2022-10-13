a598ac0d815a9729341ce135a24f959deea68e5e
zhuimengfb <fubo0324@gmail.com>
2022-10-13 17:40:02 +0800

    refactor: remove event dispatch throttle Event should be filtered by developer themselves, the frequency limit may lead to laggins

--- webf/lib/src/gesture/gesture_dispatcher.dart
+++ webf/lib/src/gesture/gesture_dispatcher.dart
@@ -9,7 +9,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:webf/dom.dart';
 import 'package:webf/gesture.dart';
-import 'package:webf/src/scheduler/throttle.dart';
 
 const int _MAX_STEP_MS = 16;
 
@@ -100,8 +99,6 @@ class GestureDispatcher {
   List<EventTarget> _eventPath = const [];
   // Collect the events in the event path list.
   final Map<String, bool> _eventsInPath = {};
-
-  final Throttling _throttler = Throttling(duration: Duration(milliseconds: _MAX_STEP_MS));
 
   final Map<int, EventTarget> _pointTargets = {};
   void _bindEventTargetWithTouchPoint(TouchPoint touchPoint, EventTarget eventTarget) {
@@ -402,13 +399,7 @@ class GestureDispatcher {
         e.touches.append(touch);
       }
 
-      if (eventType == EVENT_TOUCH_MOVE) {
-        _throttler.throttle(() {
-          _pointTargets[currentTouchPoint.id]?.dispatchEvent(e);
-        });
-      } else {
-        _pointTargets[currentTouchPoint.id]?.dispatchEvent(e);
-      }
+      _pointTargets[currentTouchPoint.id]?.dispatchEvent(e);
     }
   }
 }
