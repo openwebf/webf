0034c9894437f4d0c449d81ef2ac191575ac1259
pba-cra <syb233@icloud.com>
2022-10-14 14:56:04 +0800

    test(overflow): add test overflow change

--- integration_tests/specs/css/css-overflow/overflow-hidden.ts
+++ integration_tests/specs/css/css-overflow/overflow-hidden.ts
@@ -40,4 +40,121 @@ describe('hidden', () => {
   
     await snapshot(0.1);
   });
+
+  it('should scroll with overflow scroll', async (done) => {
+    let p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a text for test scroll logic to uset.`)]
+
+    );
+    let container = createElement('div', {
+      style: {
+        width: '80px',
+        height: '80px',
+        borderRadius: '10px',
+        overflow: "scroll",
+      }
+    }, [
+      p
+    ]);
+    document.body.appendChild(container);
+    requestAnimationFrame(async () => {
+      await simulateSwipe(0, 60, 50, 0, 0.5);
+      await snapshot(1);
+      done();
+    });
+  });
+
+  it('should can not scroll with overflow hidden', async (done) => {
+    let p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a text for test scroll logic to uset.`)]
+
+    );
+    let container = createElement('div', {
+      style: {
+        width: '80px',
+        height: '80px',
+        borderRadius: '10px',
+        overflow: "hidden",
+      }
+    }, [
+      p
+    ]);
+    document.body.appendChild(container);
+    requestAnimationFrame(async () => {
+      await simulateSwipe(0, 60, 50, 0, 0.5);
+      await snapshot(0.6);
+      done();
+    });
+  });
+
+  it('should scroll with overflow change scroll', async (done) => {
+    let p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a text for test scroll logic to uset.`)]
+    );
+    let container = createElement('div', {
+      style: {
+        width: '80px',
+        height: '80px',
+        borderRadius: '10px',
+        overflow: "hidden",
+      }
+    }, [
+      p
+    ]);
+    document.body.appendChild(container);
+
+    requestAnimationFrame(async () => {
+      container.style.overflow = 'scroll';
+      await simulateSwipe(0, 60, 50, 0, 0.5);
+      await snapshot(1);
+      done();
+    });
+  });
+
+  it('should can not scroll with overflow change hidden', async (done) => {
+    let p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a text for test scroll logic to uset.`)]
+    );
+    let container = createElement('div', {
+      style: {
+        width: '80px',
+        height: '80px',
+        borderRadius: '10px',
+        overflow: "scroll",
+      }
+    }, [
+      p
+    ]);
+    document.body.appendChild(container);
+    requestAnimationFrame(async () => {
+      container.style.overflow = 'hidden';
+      await simulateSwipe(0, 60, 50, 0, 0.5);
+      await snapshot(0.6);
+      done();
+    });
+  });
 });
