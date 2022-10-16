a271d23555f36565cc420b471f09db03a3cc52cf
xuzhongpeng <1452754335@qq.com>
2022-10-16 20:47:45 +0800

    refactor: 1. add BaseInputElement to convergence input and textarea 2. set default border and border radius for input and textarea 3. support more attribute such as autofocus

--- webf/lib/src/dom/elements/input.dart
+++ webf/lib/src/dom/elements/input.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:webf/dom.dart';
 import 'package:webf/webf.dart';
 
-enum InputSize { small, medium, large }
+enum InputSize {
+  small,
+  medium,
+  large,
+}
 
-class FlutterInputElement extends WidgetElement {
-  FlutterInputElement(BindingContext? context) : super(context);
+class FlutterInputElement extends WidgetElement
+    with BaseInputElement, BaseCheckBoxElement {
+  BindingContext? buildContext;
+  FlutterInputElement(BindingContext? context) : super(context) {
+    buildContext = context;
+  }
+  @override
+  Widget build(BuildContext context, List<Widget> children) {
+    switch (type) {
+      case 'checkbox':
+        return createCheckBox(context);
+      // TODO support more type
+      default:
+        return createInput(context);
+    }
+  }
+}
 
+/// create a base input widget containing input and textarea
+mixin BaseInputElement on WidgetElement {
   TextEditingController controller = TextEditingController();
-
-  String inputSize = 'medium';
-
-  bool checked = false;
+  String? oldValue;
 
   String get value => controller.value.text;
 
@@ -26,52 +43,8 @@ class FlutterInputElement extends WidgetElement {
     switch (key) {
       case 'value':
         return controller.value.text;
-      case 'checked':
-        return checked;
     }
     return super.getBindingProperty(key);
-  }
-
-  @override
-  void setBindingProperty(String key, value) {
-    switch (key) {
-      case 'checked':
-        checked = value;
-        break;
-      case 'value':
-        this.value = value;
-        break;
-    }
-    super.setBindingProperty(key, value);
-  }
-
-  @override
-  void setAttribute(String key, String value) {
-    switch (key) {
-      case 'size':
-        inputSize = value;
-        break;
-      case 'checked':
-        print('value:');
-        checked = value == 'true';
-        break;
-      case 'value':
-        this.value = value;
-        break;
-    }
-    super.setAttribute(key, value);
-  }
-
-  double getFontSize() {
-    switch (inputSize) {
-      case 'small':
-        return 14;
-      case 'large':
-        return 18;
-      case 'medium':
-      default:
-        return 16;
-    }
   }
 
   TextInputType? getKeyboardType() {
@@ -89,8 +62,14 @@ class FlutterInputElement extends WidgetElement {
   String get placeholder => getAttribute('placeholder') ?? '';
 
   String? get label => getAttribute('label');
-
-  bool get disabled => getAttribute('disabled') == 'disabled';
+  bool get disabled => getAttribute('disabled') != null;
+  bool get autofocus => getAttribute('autofocus') != null;
+  bool get readonly => getAttribute('readonly') != null;
+  int? get maxLength {
+    String? value = getAttribute('maxLength');
+    if (value != null) return int.parse(value);
+    return null;
+  }
 
   List<TextInputFormatter>? getInputFormatters() {
     switch (type) {
@@ -100,8 +79,13 @@ class FlutterInputElement extends WidgetElement {
     return null;
   }
 
-  Widget createInput(BuildContext context) {
-    FlutterFormElementContext? formContext = context.dependOnInheritedWidgetOfExactType<FlutterFormElementContext>();
+  Color get color => renderStyle.color;
+  double? get height => renderStyle.height.value;
+  double? get width => renderStyle.width.value;
+
+  Widget _createInputWidget(BuildContext context, int minLines, int maxLines) {
+    FlutterFormElementContext? formContext =
+        context.dependOnInheritedWidgetOfExactType<FlutterFormElementContext>();
     onChanged(String newValue) {
       setState(() {
         InputEvent inputEvent = InputEvent(inputType: '', data: newValue);
@@ -111,93 +95,164 @@ class FlutterInputElement extends WidgetElement {
 
     InputDecoration decoration = InputDecoration(
       label: label != null ? Text(label!) : null,
-      border: null,
+      border: InputBorder.none,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+      isDense: true,
       hintText: placeholder,
-      suffixIcon: controller.value.text.isNotEmpty
-          ? IconButton(
-              iconSize: 14,
-              onPressed: () {
-                setState(() {
-                  controller.clear();
-                  InputEvent inputEvent = InputEvent(inputType: '', data: '');
-                  dispatchEvent(inputEvent);
-                });
-              },
-              icon: Icon(Icons.clear),
-            )
-          : null,
     );
-
+    late Widget widget;
     if (formContext != null) {
-      return TextFormField(
+      widget = TextFormField(
         controller: controller,
-        enabled: !disabled,
+        enabled: !disabled && !readonly,
+        style: TextStyle(
+          fontFamily: renderStyle.fontFamily?.join(' '),
+          color: color,
+          fontSize: renderStyle.fontSize.value,
+        ),
+        autofocus: autofocus,
+        minLines: minLines,
+        maxLines: maxLines,
+        maxLength: maxLength,
+        onChanged: onChanged,
+        cursorColor: color,
         keyboardType: getKeyboardType(),
         inputFormatters: getInputFormatters(),
-        style: TextStyle(fontFamily: renderStyle.fontFamily?.join(' '), fontSize: getFontSize()),
+        decoration: decoration,
+      );
+    } else {
+      widget = TextField(
+        controller: controller,
+        enabled: !disabled && !readonly,
+        style: TextStyle(
+          fontFamily: renderStyle.fontFamily?.join(' '),
+          color: color,
+          fontSize: renderStyle.fontSize.value,
+        ),
+        autofocus: autofocus,
+        minLines: minLines,
+        maxLines: maxLines,
+        maxLength: maxLength,
         onChanged: onChanged,
+        cursorColor: color,
+        keyboardType: getKeyboardType(),
+        inputFormatters: getInputFormatters(),
+        onSubmitted: (String value) {
+          if (type == 'search') {
+            dispatchEvent(Event('search'));
+          }
+        },
         decoration: decoration,
       );
     }
+    return _wrapperBorder(widget);
+  }
 
-    return TextField(
-      controller: controller,
-      enabled: !disabled,
-      style: TextStyle(fontFamily: renderStyle.fontFamily?.join(' '), fontSize: getFontSize()),
-      onChanged: onChanged,
-      keyboardType: getKeyboardType(),
-      inputFormatters: getInputFormatters(),
-      onSubmitted: (String value) {
-        if (type == 'search') {
-          dispatchEvent(Event('search'));
-        }
-      },
-      decoration: decoration,
+  // set default border and border radius
+  Widget _wrapperBorder(Widget child) {
+    List<BorderSide>? borderSides = renderStyle.borderSides;
+    List<Radius>? radius = renderStyle.borderRadius;
+    return Container(
+      alignment: height != null ? Alignment.center : null,
+      decoration: BoxDecoration(
+        border: borderSides == null
+            ? Border.all(
+                width: 1,
+                color: Color.fromRGBO(118, 118, 118, 1),
+              )
+            : null,
+        borderRadius: borderSides == null && radius == null
+            ? BorderRadius.circular(4)
+            : null,
+      ),
+      child: child,
     );
   }
 
-  double getCheckboxSize() {
-    switch (inputSize) {
-      case 'small':
-        return 0.7;
-      case 'large':
-        return 1.4;
-      case 'medium':
-      default:
-        return 1;
-    }
-  }
-
-  Widget createCheckBox(BuildContext context) {
-    return Transform.scale(
-      child: Checkbox(
-          value: checked,
-          onChanged: disabled
-              ? null
-              : (bool? newValue) {
-                  setState(() {
-                    checked = newValue!;
-                    dispatchEvent(Event('change'));
-                  });
-                }),
-      scale: getCheckboxSize(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context, List<Widget> children) {
-    if (type == 'checkbox') {
-      return createCheckBox(context);
-    }
+  Widget createInput(BuildContext context,
+      {int minLines = 1, int maxLines = 1}) {
     return Focus(
       onFocusChange: (bool isFocus) {
         if (isFocus) {
           ownerDocument.focusedElement = this;
-        } else if (ownerDocument.focusedElement == this) {
-          ownerDocument.focusedElement = null;
+          oldValue = value;
+        } else {
+          if (ownerDocument.focusedElement == this) {
+            ownerDocument.focusedElement = null;
+          }
+          if (oldValue != value) {
+            dispatchEvent(Event('change'));
+          }
         }
       },
-      child: createInput(context),
+      child: _createInputWidget(context, minLines, maxLines),
+    );
+  }
+}
+
+/// create a checkBox widget when input type='checkbox'
+mixin BaseCheckBoxElement on WidgetElement {
+  bool checked = false;
+  bool get disabled => getAttribute('disabled') != null;
+
+  @override
+  void setBindingProperty(String key, value) {
+    switch (key) {
+      case 'checked':
+        checked = value;
+        break;
+    }
+    super.setBindingProperty(key, value);
+  }
+
+  @override
+  void setAttribute(String key, String value) {
+    switch (key) {
+      case 'checked':
+        checked = value == 'true';
+    }
+    super.setAttribute(key, value);
+  }
+
+  @override
+  getBindingProperty(String key) {
+    switch (key) {
+      case 'checked':
+        return checked;
+    }
+    return super.getBindingProperty(key);
+  }
+
+  double getCheckboxSize() {
+    //TODO support zoom
+    //width and height
+    if (renderStyle.width.value != null && renderStyle.height.value != null) {
+      return renderStyle.width.value! / 18.0;
+    }
+    return 1.0;
+  }
+
+  Widget createCheckBox(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: renderStyle.width.value,
+        height: renderStyle.height.value,
+        child: Transform.scale(
+          child: Checkbox(
+            value: checked,
+            onChanged: disabled
+                ? null
+                : (bool? newValue) {
+                    setState(() {
+                      checked = newValue!;
+                      dispatchEvent(Event('change'));
+                    });
+                  },
+          ),
+          scale: getCheckboxSize(),
+        ),
+      ),
     );
   }
 }
--- webf/lib/src/dom/elements/textarea.dart
+++ webf/lib/src/dom/elements/textarea.dart
@@ -4,82 +4,11 @@ import 'package:webf/dom.dart';
 
 const TEXTAREA = 'TEXTAREA';
 
-class FlutterTextAreaElement extends WidgetElement {
+class FlutterTextAreaElement extends WidgetElement with BaseInputElement {
   FlutterTextAreaElement(super.context);
-
-  TextEditingController controller = TextEditingController();
-
-  String get value => controller.value.text;
-  set value(String? value) {
-    if (value == null) return;
-    controller.value = TextEditingValue(text: value);
-  }
-
-  @override
-  getBindingProperty(String key) {
-    switch (key) {
-      case 'value':
-        return value;
-    }
-    return super.getBindingProperty(key);
-  }
-
-  @override
-  void setBindingProperty(String key, value) {
-    switch(key) {
-      case 'value':
-        this.value = value;
-        break;
-    }
-    super.setBindingProperty(key, value);
-  }
-
-  @override
-  void setAttribute(String key, String value) {
-    switch(key) {
-      case 'value':
-        this.value = value;
-        break;
-    }
-    super.setAttribute(key, value);
-  }
-
-  int? get maxLength {
-    String? value = getAttribute('maxLength');
-    if (value != null) return int.parse(value);
-    return null;
-  }
-
-  bool get disabled {
-    return getAttribute('disabled') == 'disabled';
-  }
 
   @override
   Widget build(BuildContext context, List<Widget> children) {
-    onChanged(String newValue) {
-      setState(() {
-        InputEvent inputEvent = InputEvent(inputType: '', data: newValue);
-        dispatchEvent(inputEvent);
-      });
-    }
-
-    return Focus(
-      child: TextField(
-          enabled: !disabled,
-          controller: controller,
-          decoration: InputDecoration(border: InputBorder.none),
-          onChanged: onChanged,
-          minLines: 3,
-          maxLines: 5,
-          maxLength: maxLength,
-          keyboardType: TextInputType.multiline),
-      onFocusChange: (bool isFocus) {
-        if (isFocus) {
-          ownerDocument.focusedElement = this;
-        } else if (ownerDocument.focusedElement == this) {
-          ownerDocument.focusedElement = null;
-        }
-      },
-    );
+    return createInput(context, minLines: 3, maxLines: 5);
   }
 }
