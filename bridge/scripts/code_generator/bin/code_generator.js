a47cdcc2d0b7b483f78fc18b889d14b33a83bfe1
andycall <dongtiangche@outlook.com>
2022-11-09 15:25:58 +0800

    feat: support generate union types from typings.

--- bridge/scripts/code_generator/bin/code_generator.js
+++ bridge/scripts/code_generator/bin/code_generator.js
@@ -10,8 +10,10 @@ const { JSONBlob } = require('../dist/json/JSONBlob');
 const { JSONTemplate } = require('../dist/json/JSONTemplate');
 const { analyzer } = require('../dist/idl/analyzer');
 const { generatorSource } = require('../dist/idl/generator')
+const { generateUnionTypes, generateUnionTypeFileName } = require('../dist/idl/generateUnionTypes')
 const { generateJSONTemplate } = require('../dist/json/generator');
 const { generateNamesInstaller } = require("../dist/json/generator");
+const {union} = require("lodash");
 
 program
   .version(packageJSON.version)
@@ -56,7 +58,7 @@ function genCodeFromTypeDefine() {
   // Analyze all files first.
   for (let i = 0; i < blobs.length; i ++) {
     let b = blobs[i];
-    analyzer(b, definedPropertyCollector);
+    analyzer(b, definedPropertyCollector, unionTypeCollector);
   }
 
   for (let i = 0; i < blobs.length; i ++) {
@@ -71,6 +73,14 @@ function genCodeFromTypeDefine() {
 
     wirteFileIfChanged(genFilePath + '.h', result.header);
     wirteFileIfChanged(genFilePath + '.cc', result.source);
+  }
+
+  let unionTypes = Array.from(unionTypeCollector.types);
+  for(let i = 0; i < unionTypes.length; i ++) {
+    let result = generateUnionTypes(unionTypes[i]);
+    let filename = generateUnionTypeFileName(unionTypes[i]);
+    fs.writeFileSync(path.join(dist, filename) + '.h', result.header);
+    fs.writeFileSync(path.join(dist, filename) + '.cc', result.source);
   }
 }
 
@@ -146,7 +156,12 @@ class DefinedPropertyCollector {
   interfaces = new Set();
 }
 
+class UnionTypeCollector {
+  types = new Set()
+}
+
 let definedPropertyCollector = new DefinedPropertyCollector();
+let unionTypeCollector = new UnionTypeCollector();
 let names_needs_install = new Set();
 
 genCodeFromTypeDefine();
