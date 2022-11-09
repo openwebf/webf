3fab735316b665e9160861a2b8e169fe5a0dbc44
andycall <dongtiangche@outlook.com>
2022-11-09 15:58:15 +0800

    chore: fix clang format break compile.

--- bridge/bindings/qjs/cppgc/trace_if_needed.h
+++ bridge/bindings/qjs/cppgc/trace_if_needed.h
@@ -6,11 +6,11 @@
 #define WEBF_BINDINGS_QJS_CPPGC_TRACE_IF_NEEDED_H_
 
 #include "bindings/qjs/atomic_string.h"
-#include "bindings/qjs/idl_type.h"
 #include "bindings/qjs/js_event_handler.h"
 #include "bindings/qjs/js_event_listener.h"
 #include "bindings/qjs/script_promise.h"
 #include "bindings/qjs/script_wrappable.h"
+#include "bindings/qjs/idl_type.h"
 #include "gc_visitor.h"
 #include "member.h"
 
