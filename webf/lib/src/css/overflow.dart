534bf0609a587e976228080745436f6768ed8e88
andycall <dongtiangche@outlook.com>
2022-11-21 18:17:39 +0800

    fix: fix set display: none to overflow box.

--- webf/lib/src/css/overflow.dart
+++ webf/lib/src/css/overflow.dart
@@ -189,6 +189,8 @@ mixin ElementOverflowMixin on ElementBase {
   }
 
   void scrollingContentBoxStyleListener(String property, String? original, String present) {
+    if (renderBoxModel == null) return;
+
     RenderLayoutBox? scrollingContentBox = (renderBoxModel as RenderLayoutBox).renderScrollingContent;
     // Sliver content has no multi scroll content box.
     if (scrollingContentBox == null) return;
