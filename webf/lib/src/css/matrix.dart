ee0e86941d1d593893ac15935a5011aeacaf3269
devjiangzhou <jiangzhoubai@gmail.com>
2022-12-05 23:40:38 +0800

    fix computed value

--- webf/lib/src/css/matrix.dart
+++ webf/lib/src/css/matrix.dart
@@ -614,13 +614,13 @@ class CSSMatrix {
     var methodArgs = method.args;
     for (int i = 0; i < methodArgs.length; i++) {
       String arg = methodArgs[i];
-      final calcValue = CSSCalcValue.tryParse(renderStyle, '', arg);
+      final calcValue = CSSCalcValue.tryParse(renderStyle, '', arg)?.computedValue('').toString();
       if (calcValue != null) {
-        methodArgs[i] = calcValue.computedValue('').toString();
+        methodArgs[i] = calcValue;
       } else {
-        final varValue = CSSVariable.tryParse(renderStyle, arg);
+        final varValue = CSSVariable.tryParse(renderStyle, arg)?.computedValue('').toString();
         if (varValue != null) {
-          methodArgs[i] = varValue.computedValue('');
+          methodArgs[i] = varValue;
         }
       }
     }
