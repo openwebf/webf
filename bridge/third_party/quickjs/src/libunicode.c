010688fde6a91d426bc66e65369a1cc1bf7d3e19
董天成 <dongtiangche@outlook.com>
2023-03-21 23:54:02 +0800

    feat: expand quickjs default prop size and realloc capacity. (#270)

--- bridge/third_party/quickjs/src/libunicode.c
+++ bridge/third_party/quickjs/src/libunicode.c
@@ -302,7 +302,7 @@ int cr_realloc(CharRange *cr, int size)
     uint32_t *new_buf;
     
     if (size > cr->size) {
-        new_size = max_int(size, cr->size * 3 / 2);
+        new_size = max_int(size, cr->size * 9 / 2);
         new_buf = cr->realloc_func(cr->mem_opaque, cr->points,
                                    new_size * sizeof(cr->points[0]));
         if (!new_buf)
