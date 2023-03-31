1d25a4e3fddf25e0194fee360b00ecd93651448f
andycall <dongtiangche@outlook.com>
2023-03-31 14:12:33 +0800

    feat: decompress gzip content to caches.

--- webf/lib/src/painting/cached_network_image.dart
+++ webf/lib/src/painting/cached_network_image.dart
@@ -40,10 +40,7 @@ class CachedNetworkImage extends ImageProvider<CachedNetworkImageKey> {
   final Map<String, String>? headers;
 
   // Do not access this field directly; use [_httpClient] instead.
-  // We set `autoUncompress` to false to ensure that we can trust the value of
-  // the `Content-CSSLength` HTTP header. We automatically uncompress the content
-  // in our call to [consolidateHttpClientResponseBytes].
-  static final HttpClient _sharedHttpClient = HttpClient()..autoUncompress = false;
+  static final HttpClient _sharedHttpClient = HttpClient();
 
   static HttpClient get _httpClient {
     HttpClient client = _sharedHttpClient;
@@ -99,7 +96,7 @@ class CachedNetworkImage extends ImageProvider<CachedNetworkImageKey> {
       cacheController.putObject(resolved, cacheObject);
 
       HttpClientResponse _response = HttpClientCachedResponse(response, cacheObject);
-      final Uint8List bytes = await consolidateHttpClientResponseBytes(
+      Uint8List bytes = await consolidateHttpClientResponseBytes(
         _response,
         onBytesReceived: (int cumulative, int? total) {
           chunkEvents.add(ImageChunkEvent(
@@ -108,6 +105,12 @@ class CachedNetworkImage extends ImageProvider<CachedNetworkImageKey> {
           ));
         },
       );
+
+      // To maintain compatibility with older versions of WebF, which save Gzip content in caches, we should check the bytes
+      // and decode them if they are in gzip format.
+      if (isGzip(bytes)) {
+        bytes = Uint8List.fromList(gzip.decoder.convert(bytes));
+      }
 
       if (bytes.lengthInBytes == 0) {
         HttpCacheObject cacheObject = await cacheController.getCacheObject(resolved);
