4ed1f96daa84d3cecc927312a6f428c60e3a0739
董天成 <dongtiangche@outlook.com>
2023-04-03 11:16:25 +0800

    Fix: fix scrollable size when update (#301)
    * fix: fix scrollable size didn't update when change size in fixed container.
    
    * feat: check snapshots in CI.
    
    * fix: fix renderBoxModel can be null.
    
    * fix: fix type assert on scroll container.

--- integration_tests/lib/main.dart
+++ integration_tests/lib/main.dart
@@ -45,6 +45,7 @@ void main() async {
 
   ModuleManager.defineModule((moduleManager) => DemoModule(moduleManager));
   await startHttpMockServer();
+  sleep(Duration(seconds: 2));
 
   // FIXME: This is a workaround for testcases.
   debugOverridePDefaultStyle({DISPLAY: BLOCK});
