ce507fb255ac7d6385b274151ff4a365b944e735
董天成 <dongtiangche@outlook.com>
2023-04-16 15:45:34 +0800

    feat: add prepare scripts before windows build.

--- webf/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ webf/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 import webf
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
