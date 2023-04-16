823514ef7e7785241dbbddb3d8997ecdf9dc511d
董天成 <dongtiangche@outlook.com>
2023-04-16 15:45:34 +0800

    feat: add prepare scripts before windows build.

---
+++ webf/windows/prepare.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+function replaceTextToDLL(p) {
+    const fileExt = p.slice(-3);
+    p = path.isAbsolute(p) ? p : path.join(__dirname, p);
+    if (fileExt === 'txt' && fs.existsSync(p)) {
+        const relPath = fs.readFileSync(p, {encoding: 'utf-8'});
+        const targetPath = path.join(__dirname, relPath);
+        fs.createReadStream(targetPath.trim()).pipe(fs.createWriteStream(p.replace('.txt', '.dll').trim()))
+    }
+}
+
+replaceTextToDLL('./webf.txt');
+replaceTextToDLL('./pthread.txt');
+replaceTextToDLL('./quickjs.txt');
