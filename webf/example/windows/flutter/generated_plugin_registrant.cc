12fcb98fa9733feb0c1dda1cd2bd28b77525c38d
董天成 <dongtiangche@outlook.com>
2023-04-16 18:56:06 +0800

    fix: fix dll import.

--- webf/example/windows/flutter/generated_plugin_registrant.cc
+++ webf/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <webf/webf_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
