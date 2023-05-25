7d3c7ab613a691e5d89cdec8b4a07553c84177f0
andycall <dongtiangche@outlook.com>
2023-05-25 13:31:11 +0800

    chore: remove polyfill patch for vue.

--- bridge/polyfill/src/dom.ts
+++ bridge/polyfill/src/dom.ts
@@ -12,17 +12,6 @@ document.documentElement.appendChild(head);
 let body = document.createElement('body');
 document.documentElement.appendChild(body);
 
-// @ts-ignore
-class SVGElement extends Element {
-  constructor() {
-    super();
-  }
-}
-
-Object.defineProperty(window, 'SVGElement', {
-  value: SVGElement
-});
-
 // SVGMatrix are equal to DOMMatrix.
 Object.defineProperty(window, 'SVGMatrix', {
   value: DOMMatrix
