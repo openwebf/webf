41c9179f3803c19eaeab26dacdb86d8fb82c23a0
andycall <dongtiangche@outlook.com>
2023-07-23 13:57:15 +0800

    fix: fix Node.insertBefore with SVGElement error.

--- integration_tests/specs/dom/nodes/insert-before.ts
+++ integration_tests/specs/dom/nodes/insert-before.ts
@@ -212,4 +212,32 @@ describe('Insert before', () => {
 
     await snapshot();
   });
+
+  it('insert before when new child is SVG element', async () => {
+    var text1 = document.createTextNode('Hello webf!');
+    var br = document.createElement('br');
+    var text2 = document.createTextNode('你好，webf！');
+    var p = document.createElement('p');
+    p.className = 'p';
+    p.style.display = 'inline-block';
+    p.style.textAlign = 'center';
+    p.style.animation = '3s ease-in 1s 1 reverse both running example';
+    p.appendChild(text1);
+    p.appendChild(br);
+    p.appendChild(text2);
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+
+    document.body.appendChild(p);
+    p.appendChild(div);
+
+    const svg_wrapper = document.createElement('div');
+    svg_wrapper.innerHTML = `<svg class="icon" viewBox="0 0 1024 1024"><path d="M64 512c0 195.2 124.8 361.6 300.8 422.4 22.4 6.4 19.2-9.6 19.2-22.4v-76.8c-134.4 16-140.8-73.6-150.4-89.6-19.2-32-60.8-38.4-48-54.4 32-16 64 3.2 99.2 57.6 25.6 38.4 76.8 32 105.6 25.6 6.4-22.4 19.2-44.8 35.2-60.8-144-22.4-201.6-108.8-201.6-211.2 0-48 16-96 48-131.2-22.4-60.8 0-115.2 3.2-121.6 57.6-6.4 118.4 41.6 124.8 44.8 32-9.6 70.4-12.8 112-12.8 41.6 0 80 6.4 112 12.8 12.8-9.6 67.2-48 121.6-44.8 3.2 6.4 25.6 57.6 6.4 118.4 32 38.4 48 83.2 48 131.2 0 102.4-57.6 188.8-201.6 214.4 22.4 22.4 38.4 54.4 38.4 92.8v112c0 9.6 0 19.2 16 19.2C832 876.8 960 710.4 960 512c0-246.4-201.6-448-448-448S64 265.6 64 512z" fill="#040000" p-id="3824"></path></svg>`;
+    const svg = svg_wrapper.firstChild as SVGElement;
+    p.insertBefore(svg, div);
+
+    await snapshot();
+  });
 });
