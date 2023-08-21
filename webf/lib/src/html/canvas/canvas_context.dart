ea5c29c9f40324a7a9894dc08ea3e4b46d85f31d
jinwencan <jwc0831@163.com>
2023-08-21 23:29:01 +0800

    feat: support CanvasRenderingContext2D: createPattern() method

--- webf/lib/src/html/canvas/canvas_context.dart
+++ webf/lib/src/html/canvas/canvas_context.dart
@@ -21,14 +21,7 @@ enum CanvasLineJoin { round, bevel, miter }
 
 enum CanvasTextAlign { start, end, left, right, center }
 
-enum CanvasTextBaseline {
-  top,
-  hanging,
-  middle,
-  alphabetic,
-  ideographic,
-  bottom
-}
+enum CanvasTextBaseline { top, hanging, middle, alphabetic, ideographic, bottom }
 
 enum CanvasDirection { ltr, rtl, inherit }
 
@@ -86,8 +79,7 @@ abstract class CanvasCompositing {
 abstract class CanvasImageSmoothing {
   // image smoothing
   bool imageSmoothingEnabled = true; // (default true)
-  ImageSmoothingQuality imageSmoothingQuality =
-      ImageSmoothingQuality.low; // (default low)
+  ImageSmoothingQuality imageSmoothingQuality = ImageSmoothingQuality.low; // (default low)
 }
 
 class CanvasImageSource {
@@ -98,21 +90,23 @@ class CanvasImageSource {
   void _fillCanvasImageSource(source) {
     if (source is ImageElement) {
       image_element = source;
+    } else if (source is CanvasElement) {
+      canvas_element = source;
     }
   }
 
   ImageElement? image_element;
+
+  CanvasElement? canvas_element;
 }
 
 abstract class CanvasFillStrokeStyles {
   // colors and styles (see also the CanvasPathDrawingStyles and CanvasTextDrawingStyles
   Color strokeStyle = Color(0xFF000000); // (default black)
   Color fillStyle = Color(0xFF000000); // (default black)
-  CanvasGradient createLinearGradient(
-      double x0, double y0, double x1, double y1);
+  CanvasGradient createLinearGradient(double x0, double y0, double x1, double y1);
 
-  CanvasGradient createRadialGradient(
-      double x0, double y0, double r0, double x1, double y1, double r1);
+  CanvasGradient createRadialGradient(double x0, double y0, double r0, double x1, double y1, double r1);
 
   CanvasPattern createPattern(CanvasImageSource image, String repetition);
 }
@@ -153,6 +147,7 @@ class CanvasGradient extends BindingObject {
   get pointer => _pointer;
 
   List<CSSColorStop> colorGradients = [];
+
   // opaque object
   void addColorStop(num offset, String color) {
     Color? colorStop = CSSColor.parseColor(color, renderStyle: ownerCanvasElement.renderStyle);
@@ -163,9 +158,8 @@ class CanvasGradient extends BindingObject {
 
   @override
   void initializeMethods(Map<String, BindingObjectMethod> methods) {
-    methods['addColorStop'] = BindingObjectMethodSync(
-        call: (args) => addColorStop(
-            castToType<num>(args[0]), castToType<String>(args[1])));
+    methods['addColorStop'] =
+        BindingObjectMethodSync(call: (args) => addColorStop(castToType<num>(args[0]), castToType<String>(args[1])));
   }
 
   @override
@@ -176,9 +170,19 @@ class CanvasGradient extends BindingObject {
 class CanvasPattern extends BindingObject {
   CanvasPattern(CanvasImageSource image, String repetition)
       : _pointer = allocateNewBindingObject(),
+        _image = image,
+        _repetition = repetition,
         super();
 
   final ffi.Pointer<NativeBindingObject> _pointer;
+
+  final String _repetition;
+
+  final CanvasImageSource _image;
+
+  String get repetition => _repetition;
+
+  CanvasImageSource get image => _image;
 
   @override
   get pointer => _pointer;
