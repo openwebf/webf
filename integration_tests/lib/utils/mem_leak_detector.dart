bbbf707333a4f7af30c42aada4b6d3964d8464e6
andycall <dongtiangche@outlook.com>
2023-09-22 22:31:35 +0800

    test: add memory leak integration test.

--- integration_tests/lib/utils/mem_leak_detector.dart
+++ integration_tests/lib/utils/mem_leak_detector.dart
@@ -1,57 +1,25 @@
 import 'dart:math';
 
-double findSlope(List<List<num>> coordinates) {
-  if (coordinates.length < 2) {
-    throw ArgumentError("At least two coordinates are required");
+double linearRegressionSlope(List<double> x, List<double> y) {
+  int n = x.length;
+  double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+
+  for (int i = 0; i < n; i++) {
+    sumX += x[i];
+    sumY += y[i];
+    sumXY += x[i] * y[i];
+    sumX2 += x[i] * x[i];
   }
 
-  int n = coordinates.length;
-  num sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
-
-  for (List<num> coordinate in coordinates) {
-    num x = coordinate[0];
-    num y = coordinate[1];
-
-    sumX += x;
-    sumY += y;
-    sumXY += x * y;
-    sumX2 += x * x;
-  }
-
-  // Calculate the slope of the best-fitting line using the linear regression formula
-  double slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
-  return slope;
+  return (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
 }
 
-double findAngle(List<List<num>> coordinates) {
-  double slope = findSlope(coordinates);
-  double angleRadians = atan(slope); // Calculate angle in radians
-  return angleRadians;
+bool detectMemoryLeakBasedOnRegression(List<double> memoryUsage) {
+  List<double> timePoints = List.generate(memoryUsage.length, (index) => index.toDouble()); // [0.0, 1.0, 2.0, ...]
+  double slope = linearRegressionSlope(timePoints, memoryUsage);
+  return slope > 5; // 如果斜率大于0，则判断为可能存在内存泄漏
 }
 
-double radiansToDegrees(double radians) {
-  return radians * 180 / pi;
-}
-
-int findMinValue(List<int> numbers) {
-  if (numbers.isEmpty) {
-    throw ArgumentError("List must not be empty");
-  }
-
-  int minValue = numbers[0];
-  for (int number in numbers) {
-    minValue = minValue > number ? number : minValue;
-  }
-
-  return minValue;
-}
-
-bool isMemLeaks(List<List<int>> mems) {
-  List<List<int>> input = mems.map((e) => [e[0], e[1]]).toList();
-  double angleRadians = findAngle(input);
-  double angleDegrees = radiansToDegrees(angleRadians);
-
-  print(input);
-  print(angleDegrees);
-  return angleDegrees > 30;
+bool isMemLeaks(List<double> mems) {
+  return detectMemoryLeakBasedOnRegression(mems);
 }
