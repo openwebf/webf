1978e489f78a43be5922fde9d8702e346cddca66
andycall <dongtiangche@outlook.com>
2023-10-06 20:34:01 +0800

    fix: fix build error

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -123,10 +123,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/openwebf/webf',
