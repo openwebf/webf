32dc084bf2f67c2e6a9894db36a633ec94451fc7
Ivan-young66 <yisong86@outlook.com>
2023-10-29 14:55:16 +0400

    fix: fix touchList null

--- bridge/core/events/touch_event.d.ts
+++ bridge/core/events/touch_event.d.ts
@@ -5,9 +5,9 @@ import {TouchEventInit} from "./touch_event_init";
 
 /** An event sent when the state of contacts with a touch-sensitive surface changes. This surface can be a touch screen or trackpad, for example. The event can describe one or more points of contact with the screen and includes support for detecting movement, addition and removal of contact points, and so forth. */
 interface TouchEvent extends UIEvent {
-    readonly touches: TouchList;
-    readonly targetTouches: TouchList;
-    readonly changedTouches: TouchList;
+    readonly touches: TouchList | null;
+    readonly targetTouches: TouchList | null;
+    readonly changedTouches: TouchList | null;
     readonly altKey: boolean;
     readonly metaKey: boolean;
     readonly ctrlKey: boolean;
