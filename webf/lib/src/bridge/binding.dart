06d4b1524537c793a343b467b443d8885e85062a
andycall <dongtiangche@outlook.com>
2023-11-13 11:58:37 +0800

    fix: add checks for currentTarget.

--- webf/lib/src/bridge/binding.dart
+++ webf/lib/src/bridge/binding.dart
@@ -49,7 +49,12 @@ void _dispatchEventToNative(Event event, bool isCapture) {
   Pointer<NativeBindingObject>? pointer = event.currentTarget?.pointer;
   int? contextId = event.target?.contextId;
   WebFController controller = WebFController.getControllerOfJSContextId(contextId)!;
-  if (contextId != null && pointer != null && pointer.ref.invokeBindingMethodFromDart != nullptr && event.target?.disposed != true) {
+  if (contextId != null &&
+      pointer != null &&
+      pointer.ref.invokeBindingMethodFromDart != nullptr &&
+      event.target?.pointer?.ref.disposed != true &&
+      event.currentTarget?.pointer?.ref.disposed != true
+  ) {
     BindingObject bindingObject = controller.view.getBindingObject(pointer);
     // Call methods implements at C++ side.
     DartInvokeBindingMethodsFromDart f = pointer.ref.invokeBindingMethodFromDart.asFunction();
