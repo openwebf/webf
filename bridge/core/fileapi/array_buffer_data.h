0b5ffc9dfa5b2f888dd356d68fddf42c9edd192f
femalemonkeyman <37908962+femalemonkeyman@users.noreply.github.com>
2023-12-11 17:30:15 +0000

    fix: add includes for cstdint to allow building on ArchLinux

--- bridge/core/fileapi/array_buffer_data.h
+++ bridge/core/fileapi/array_buffer_data.h
@@ -2,6 +2,7 @@
  * Copyright (C) 2019-2022 The Kraken authors. All rights reserved.
  * Copyright (C) 2022-present The WebF authors. All rights reserved.
  */
+#include <cstdint>
 #ifndef BRIDGE_CORE_FILEAPI_ARRAY_BUFFER_DATA_H_
 #define BRIDGE_CORE_FILEAPI_ARRAY_BUFFER_DATA_H_
 
