0473e68e8ad99eef6a0e3fa666b1ad2aff1e5a3c
andycall <dongtiangche@outlook.com>
2024-02-17 06:48:13 +0800

    feat: compile and running on macos works.

--- bridge/core_rs/src/dom.rs
+++ bridge/core_rs/src/dom.rs
@@ -6,30 +6,21 @@ use std::ffi::{CString};
 use webf_sys::executing_context::ExecutingContext;
 use webf_sys::node::NodeMethods;
 
-pub fn init_webf_dom(context: &ExecutingContext) {
+pub fn init_webf_dom(context: &ExecutingContext){
   let document = context.document();
   let exception_state = context.create_exception_state();
 
   let html_tag_name = CString::new("html");
-  let html_element = document.create_element(&html_tag_name.unwrap(), &exception_state).unwrap();
+  let html_element = document.create_element(&html_tag_name.unwrap(), &exception_state)?;
 
-  let _ = document.append_child(&html_element, &exception_state);
+  document.append_child(&html_element, &exception_state).unwrap();
 
   let head_tag_name = CString::new("head");
   let head_element = document.create_element(&head_tag_name.unwrap(), &exception_state).unwrap();
-  let _ = document.document_element().append_child(&head_element, &exception_state);
+  document.document_element().append_child(&head_element, &exception_state).unwrap();
 
   let body_tag_name = CString::new("body");
   let body_element = document.create_element(&body_tag_name.unwrap(), &exception_state).unwrap();
-  let _ = document.document_element().append_child(&body_element, &exception_state);
-
-  // let exception_state = unsafe {
-  //   ExceptionState {
-  //     ptr: webf__create_exception_state()
-  //   }
-  // };
-  // document.createElement("1234", &exception_state);
-
-  // return document;
+  document.document_element().append_child(&body_element, &exception_state).unwrap();
 }
 
