c13ac9955028b44c917f7639b206525f3d7f1c6d
andycall <dongtiangche@outlook.com>
2024-02-17 00:05:44 +0800

    fix: fix build scripts.

--- bridge/core_rs/src/lib.rs
+++ bridge/core_rs/src/lib.rs
@@ -1,8 +1,7 @@
 use std::ffi::c_void;
 use libc::{c_char, c_uint};
-use webf_sys::RustValue;
-use webf::{initialize_webf_api};
-use webf::executing_context::{ExecutingContextRustMethods};
+use webf_sys::executing_context::ExecutingContextRustMethods;
+use webf_sys::{initialize_webf_api, RustValue};
 use crate::dom::init_webf_dom;
 
 mod dom;
