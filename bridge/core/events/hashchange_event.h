3dde698f619d13a8842034c6a6f6b6af1e884d85
andycall <dongtiangche@outlook.com>
2024-03-11 23:14:03 +0800

    feat: support hash router.

---
+++ bridge/core/events/hashchange_event.h
@@ -0,0 +1,51 @@
+/*
+* Copyright (C) 2022-present The WebF authors. All rights reserved.
+*/
+
+#ifndef WEBF_CORE_EVENTS_HASHCHANGE_EVENT_H_
+#define WEBF_CORE_EVENTS_HASHCHANGE_EVENT_H_
+
+#include "bindings/qjs/dictionary_base.h"
+#include "bindings/qjs/source_location.h"
+#include "core/dom/events/event.h"
+#include "qjs_hashchange_event_init.h"
+
+namespace webf {
+
+struct NativeHashchangeEvent;
+
+class HashchangeEvent : public Event {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  using ImplType = HashchangeEvent*;
+
+  static HashchangeEvent* Create(ExecutingContext* context, const AtomicString& type, ExceptionState& exception_state);
+
+  static HashchangeEvent* Create(ExecutingContext* context,
+                              const AtomicString& type,
+                              const std::shared_ptr<HashchangeEventInit>& initializer,
+                              ExceptionState& exception_state);
+
+  explicit HashchangeEvent(ExecutingContext* context, const AtomicString& type, ExceptionState& exception_state);
+
+  explicit HashchangeEvent(ExecutingContext* context,
+                        const AtomicString& type,
+                        const std::shared_ptr<HashchangeEventInit>& initializer,
+                        ExceptionState& exception_state);
+
+  explicit HashchangeEvent(ExecutingContext* context, const AtomicString& type, NativeHashchangeEvent* native_hash_change_event);
+
+  const AtomicString& newURL() const;
+  const AtomicString& oldURL() const;
+
+  bool IsHashChangeEvent() const override;
+
+ private:
+  AtomicString new_url_;
+  AtomicString old_url_;
+};
+
+}
+
+#endif  // WEBF_CORE_EVENTS_HASHCHANGE_EVENT_H_
--- webf/lib/src/module/history.dart
+++ webf/lib/src/module/history.dart
@@ -31,6 +31,10 @@ class HistoryModule extends BaseModule {
     }
   }
 
+  bool _isFragmentItem(HistoryItem item) {
+    return Uri.parse(item.bundle.url).hasFragment;
+  }
+
   void add(WebFBundle bundle) {
     HistoryItem history = HistoryItem(bundle, null, true);
     _addItem(history);
@@ -52,6 +56,11 @@ class HistoryModule extends BaseModule {
       _nextStack.addFirst(currentItem);
       dynamic state = _previousStack.first.state;
       _dispatchPopStateEvent(state);
+
+      String previousUrl = currentItem.bundle.url;
+      if (_isFragmentItem(currentItem)) {
+        _dispatchHashChangeEvent(previousUrl, _previousStack.first.bundle.url);
+      }
     }
   }
 
@@ -94,6 +103,11 @@ class HistoryModule extends BaseModule {
   void _dispatchPopStateEvent(state) {
     PopStateEvent popStateEvent = PopStateEvent(state: state);
     moduleManager!.controller.view.window.dispatchEvent(popStateEvent);
+  }
+
+  void _dispatchHashChangeEvent(String oldUrl, String newUrl) {
+    HashChangeEvent hashChangeEvent = HashChangeEvent(oldUrl: oldUrl, newUrl: newUrl);
+    moduleManager!.controller.view.window.dispatchEvent(hashChangeEvent);
   }
 
   void pushState(state, {String? url, String? title}) {
