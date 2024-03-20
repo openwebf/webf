09f9485e6a446d62c32b52d6e8d0e92dade6b4ae
edmond.l <edmond.l@binance.com>
2024-03-20 18:07:04 +0800

    feat: support Path2D to js

--- bridge/core/binding_object.h
+++ bridge/core/binding_object.h
@@ -70,7 +70,10 @@ enum BindingMethodCallOperations {
   kAsyncAnonymousFunction,
 };
 
-enum CreateBindingObjectType { kCreateDOMMatrix = 0 };
+enum CreateBindingObjectType { 
+  kCreateDOMMatrix = 0,
+  kCreatePath2D = 1,
+};
 
 struct BindingObjectPromiseContext : public DartReadable {
   ExecutingContext* context;
