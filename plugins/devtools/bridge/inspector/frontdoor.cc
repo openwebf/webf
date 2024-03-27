8b92eb3a9b6cfdd983f4132428d7bed0409533f7
chenghuai <dongtiangche@outlook.com>
2024-03-27 16:59:23 +0800

    Delete plugins directory (#587)
    the webf/plugins/devtools directory was already dedicated for 2 years
    and no longer for use.

--- plugins/devtools/bridge/inspector/frontdoor.cc
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2020-present The Kraken authors. All rights reserved.
- */
-
-#include "inspector/frontdoor.h"
-
-namespace kraken::debugger {
-
-} // namespace kraken::debugger
