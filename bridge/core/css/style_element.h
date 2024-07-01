b4d8914344385cca13afd085d7bb546e23926c6c
xiezuobing <xiezuobing@didiglobal.com>
2024-07-01 14:56:01 +0800

    feat: add link element for rel=stylesheet(style_element)

---
+++ bridge/core/css/style_element.h
@@ -0,0 +1,69 @@
+//
+// Created by 谢作兵 on 07/06/24.
+//
+
+#ifndef WEBF_STYLE_ELEMENT_H
+#define WEBF_STYLE_ELEMENT_H
+
+#include "core/dom/element.h"
+#include "core/css/pending_sheet_type.h"
+#include "core/platform/text/text_position.h"
+#include "core/base/render_blocking_behavior.h"
+
+namespace webf {
+
+class ContainerNode;
+class Document;
+class Element;
+class Node;
+
+class StyleElement {
+ public:
+  enum ProcessingResult { kProcessingSuccessful, kProcessingFatalError };
+  StyleElement(Document*, bool created_by_parser);
+  virtual ~StyleElement();
+  void Trace(GCVisitor*) const;
+
+ protected:
+
+  virtual const AtomicString& type() const = 0;
+  virtual const AtomicString& media() const = 0;
+//
+//  // Returns whether |this| and |node| are the same object. Helps us verify
+//  // parameter validity in certain member functions with an Element parameter
+//  // which should only be called by a subclass with |this|.
+  virtual bool IsSameObject(const Node& node) const = 0;
+
+  CSSStyleSheet* sheet() const { return sheet_.Get(); }
+
+  bool IsLoading() const;
+  bool SheetLoaded(Document&);
+  void SetToPendingState(Document&, Element& element);
+
+  void RemovedFrom(Element&, ContainerNode& insertion_point);
+  void BlockingAttributeChanged(Element&);
+  ProcessingResult ProcessStyleSheet(Document&, Element&);
+  ProcessingResult ChildrenChanged(Element&);
+  ProcessingResult FinishParsingChildren(Element&);
+
+  Member<CSSStyleSheet> sheet_;
+
+ protected:
+  bool CreatedByParser() const { return created_by_parser_; }
+
+ private:
+  ProcessingResult CreateSheet(Element&, const AtomicString& text = AtomicString());
+  ProcessingResult Process(Element&);
+  void ClearSheet(Element& owner_element);
+
+  bool has_finished_parsing_children_ : 1;
+  bool loading_ : 1;
+  bool registered_as_candidate_ : 1;
+  bool created_by_parser_ : 1;
+  TextPosition start_position_;
+  PendingSheetType pending_sheet_type_;
+  RenderBlockingBehavior render_blocking_behavior_;
+};
+}  // namespace webf
+
+#endif  // WEBF_STYLE_ELEMENT_H
