5ef04379eb824f10a646d3919d8b2a19fd65b1c7
xiezuobing <xiezuobing@didiglobal.com>
2024-07-01 15:15:23 +0800

    feat: style_reclac_context and style_resolver_state

---
+++ bridge/core/css/resolver/style_resolver_state.cc
@@ -0,0 +1,7 @@
+//
+// Created by 谢作兵 on 27/06/24.
+//
+
+#include "style_resolver_state.h"
+
+namespace webf {}  // namespace webf
---
+++ bridge/core/css/style_recalc_context.cc
@@ -0,0 +1,16 @@
+//
+// Created by 谢作兵 on 27/06/24.
+//
+
+#include "style_recalc_context.h"
+
+//#include "core/css/container_query_evaluator.h"
+//#include "core/dom/layout_tree_builder_traversal.h"
+//#include "core/dom/node_computed_style.h"
+//#include "core/html/html_slot_element.h"
+
+namespace webf {
+
+
+
+}  // namespace webf
