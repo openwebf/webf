5a25ba9ee2603b51474c544acee8d92f3f1b57e6
LeuisKen <leuisken@gmail.com>
2024-07-02 19:51:56 +0200

    feat: Add new document methods binding - createElement with options version. need new method name since rs don't have override - createElementNS and its with options version.

--- bridge/core/rust_api/document.cc
+++ bridge/core/rust_api/document.cc
@@ -29,6 +29,82 @@ RustValue<Element, ElementRustMethods> DocumentRustMethods::CreateElement(
   return {.value = new_element, .method_pointer = To<ElementRustMethods>(new_element->rustMethodPointer())};
 }
 
+RustValue<Element, ElementRustMethods> DocumentRustMethods::CreateElementWithElementCreationOptions(
+    webf::Document* ptr,
+    const char* tag_name,
+    RustElementCreationOptions& options,
+    webf::SharedExceptionState* shared_exception_state) {
+  auto* document = static_cast<webf::Document*>(ptr);
+  MemberMutationScope scope{document->GetExecutingContext()};
+  webf::AtomicString tag_name_atomic = webf::AtomicString(document->ctx(), tag_name);
+
+  std::string value = std::string("{\"is\":\"") + options.is + "\"}";
+  const char* value_cstr = value.c_str();
+  webf::ScriptValue options_value = webf::ScriptValue::CreateJsonObject(document->ctx(), value_cstr, value.length());
+
+  Element* new_element = document->createElement(
+    tag_name_atomic,
+    options_value,
+    shared_exception_state->exception_state
+  );
+  if (shared_exception_state->exception_state.HasException()) {
+    return {.value = nullptr, .method_pointer = nullptr};
+  }
+
+  // Hold the reference until rust side notify this element was released.
+  new_element->KeepAlive();
+  return {.value = new_element, .method_pointer = To<ElementRustMethods>(new_element->rustMethodPointer())};
+}
+
+RustValue<Element, ElementRustMethods> DocumentRustMethods::CreateElementNS(
+    webf::Document* ptr,
+    const char* uri,
+    const char* tag_name,
+    webf::SharedExceptionState* shared_exception_state) {
+  auto* document = static_cast<webf::Document*>(ptr);
+  MemberMutationScope scope{document->GetExecutingContext()};
+  webf::AtomicString uri_atomic = webf::AtomicString(document->ctx(), uri);
+  webf::AtomicString tag_name_atomic = webf::AtomicString(document->ctx(), tag_name);
+  Element* new_element = document->createElementNS(uri_atomic, tag_name_atomic, shared_exception_state->exception_state);
+  if (shared_exception_state->exception_state.HasException()) {
+    return {.value = nullptr, .method_pointer = nullptr};
+  }
+
+  // Hold the reference until rust side notify this element was released.
+  new_element->KeepAlive();
+  return {.value = new_element, .method_pointer = To<ElementRustMethods>(new_element->rustMethodPointer())};
+}
+
+RustValue <Element, ElementRustMethods> DocumentRustMethods::CreateElementNSWithElementCreationOptions(
+    webf::Document* ptr,
+    const char* uri,
+    const char* tag_name,
+    RustElementCreationOptions& options,
+    webf::SharedExceptionState* shared_exception_state) {
+  auto* document = static_cast<webf::Document*>(ptr);
+  MemberMutationScope scope{document->GetExecutingContext()};
+  webf::AtomicString uri_atomic = webf::AtomicString(document->ctx(), uri);
+  webf::AtomicString tag_name_atomic = webf::AtomicString(document->ctx(), tag_name);
+
+  std::string value = std::string("{\"is\":\"") + options.is + "\"}";
+  const char* value_cstr = value.c_str();
+  webf::ScriptValue options_value = webf::ScriptValue::CreateJsonObject(document->ctx(), value_cstr, value.length());
+
+  Element* new_element = document->createElementNS(
+    uri_atomic,
+    tag_name_atomic,
+    options_value,
+    shared_exception_state->exception_state
+  );
+  if (shared_exception_state->exception_state.HasException()) {
+    return {.value = nullptr, .method_pointer = nullptr};
+  }
+
+  // Hold the reference until rust side notify this element was released.
+  new_element->KeepAlive();
+  return {.value = new_element, .method_pointer = To<ElementRustMethods>(new_element->rustMethodPointer())};
+}
+
 RustValue<Text, TextNodeRustMethods> DocumentRustMethods::CreateTextNode(
     webf::Document* ptr,
     const char* data,
