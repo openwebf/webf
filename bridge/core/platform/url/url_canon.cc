6ab2807e7562c42cdcbcec7170d046c4044a2722
xiezuobing <xiezuobing@didiglobal.com>
2024-08-15 11:26:14 +0800

    feat: add url canon handlers.

---
+++ bridge/core/platform/url/url_canon.cc
@@ -0,0 +1,17 @@
+// Copyright 2013 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Copyright (C) 2022-present The WebF authors. All rights reserved.
+
+#include "url_canon.h"
+
+namespace webf {
+
+namespace url {
+
+//template class CanonOutputT<char>;
+//template class CanonOutputT<char16_t>;
+
+}  // namespace url
+}  // namespace webf
