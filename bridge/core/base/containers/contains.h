4698066a1f914f389f24adf062ca5ce2d276ea5c
xiezuobing <xiezuobing@didiglobal.com>
2024-08-15 11:22:23 +0800

    feat: add base/contains

---
+++ bridge/core/base/containers/contains.h
@@ -0,0 +1,68 @@
+// Copyright 2020 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * Copyright (C) 2022-present The WebF authors. All rights reserved.
+ */
+
+#ifndef WEBF_CONTAINS_H
+#define WEBF_CONTAINS_H
+
+#include <ranges>
+#include <type_traits>
+#include <utility>
+
+#include "core/base/ranges/algorithm.h"
+//#include "core/base/ranges/ranges.h"
+
+
+namespace webf {
+
+namespace internal {
+
+// Small helper to detect whether a given type has a nested `key_type` typedef.
+// Used below to catch misuses of the API for associative containers.
+template <typename T>
+concept HasKeyType = requires { typename T::key_type; };
+
+}  // namespace internal
+
+// A general purpose utility to check whether `container` contains `value`. This
+// will probe whether a `contains` or `find` member function on `container`
+// exists, and fall back to a generic linear search over `container`.
+template <typename Container, typename Value>
+constexpr bool Contains(const Container& container, const Value& value) {
+  if constexpr (requires {
+                  { container.contains(value) } -> std::same_as<bool>;
+                }) {
+    return container.contains(value);
+  } else if constexpr (requires { container.find(value) != Container::npos; }) {
+    return container.find(value) != Container::npos;
+  } else if constexpr (requires { container.find(value) != container.end(); }) {
+    return container.find(value) != container.end();
+  } else {
+    static_assert(
+        !internal::HasKeyType<Container>,
+        "Error: About to perform linear search on an associative container. "
+        "Either use a more generic comparator (e.g. std::less<>) or, if a "
+        "linear search is desired, provide an explicit projection parameter.");
+    return ranges::find(container, value) != std::ranges::end(container);
+  }
+}
+
+// Overload that allows to provide an additional projection invocable. This
+// projection will be applied to every element in `container` before comparing
+// it with `value`. This will always perform a linear search.
+template <typename Container, typename Value, typename Proj>
+constexpr bool Contains(const Container& container,
+                        const Value& value,
+                        Proj proj) {
+  return ranges::find(container, value, std::move(proj)) !=
+         std::ranges::end(container);
+}
+
+}  // namespace webf
+
+
+#endif  // WEBF_CONTAINS_H
