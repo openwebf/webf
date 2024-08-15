c7482c7dda67fe42c46a69afbb4edaf6caf473e0
xiezuobing <xiezuobing@didiglobal.com>
2024-08-15 11:31:52 +0800

    chore: add copyright.

--- bridge/core/platform/url/url_parse_internal.h
+++ bridge/core/platform/url/url_parse_internal.h
@@ -1,6 +1,8 @@
-//
-// Created by 谢作兵 on 12/08/24.
-//
+// Copyright 2013 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Copyright (C) 2022-present The WebF authors. All rights reserved.
 
 #ifndef WEBF_URL_PARSE_INTERNAL_H
 #define WEBF_URL_PARSE_INTERNAL_H
