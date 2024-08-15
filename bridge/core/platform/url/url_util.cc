afa7cc0e50a7f6f474e814d15fe8368ea931228c
xiezuobing <xiezuobing@didiglobal.com>
2024-08-15 11:36:28 +0800

    chore: add copyright.

--- bridge/core/platform/url/url_util.cc
+++ bridge/core/platform/url/url_util.cc
@@ -1,3 +1,8 @@
+// Copyright 2013 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Copyright (C) 2022-present The WebF authors. All rights reserved.
 
 #include "url_util.h"
 
