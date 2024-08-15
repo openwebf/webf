4f0db59f836cd0e099638cd4aba19d42a3410e83
xiezuobing <xiezuobing@didiglobal.com>
2024-08-15 17:32:20 +0800

    feat: add css_pending_system_font_value without resolve hanlder.

---
+++ bridge/core/css/css_pending_system_font_value.cc
@@ -0,0 +1,49 @@
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Copyright (C) 2022-present The WebF authors. All rights reserved.
+
+#include "css_pending_system_font_value.h"
+
+#include "core/css/parser/css_parser_fast_path.h"
+//#include "core/layout/layout_theme_font_provider.h"
+
+namespace webf {
+
+namespace cssvalue {
+
+CSSPendingSystemFontValue::CSSPendingSystemFontValue(CSSValueID system_font_id)
+    : CSSValue(kPendingSystemFontValueClass), system_font_id_(system_font_id) {
+  DCHECK(CSSParserFastPaths::IsValidSystemFont(system_font_id));
+}
+
+// static
+std::shared_ptr<CSSPendingSystemFontValue> CSSPendingSystemFontValue::Create(
+    CSSValueID system_font_id) {
+  return std::make_shared<CSSPendingSystemFontValue>(system_font_id);
+}
+
+const AtomicString& CSSPendingSystemFontValue::ResolveFontFamily() const {
+  // TODO(xiezuobing): resolve font-family
+//  return LayoutThemeFontProvider::SystemFontFamily(system_font_id_);
+}
+
+float CSSPendingSystemFontValue::ResolveFontSize(
+    const Document* document) const {
+  // TODO(xiezuobing):resolve font-size
+//  return LayoutThemeFontProvider::SystemFontSize(system_font_id_, document);
+}
+
+std::string CSSPendingSystemFontValue::CustomCSSText() const {
+  return "";
+}
+
+void CSSPendingSystemFontValue::TraceAfterDispatch(
+    GCVisitor* visitor) const {
+  CSSValue::TraceAfterDispatch(visitor);
+}
+
+}  // namespace cssvalue
+
+}  // namespace webf
