dc39e64af31ec0c24e3bc6758b1dc5f1024cf623
david <>
2024-08-23 13:20:08 +0800

    feat: call v8 InstallFunctions

--- bridge/bindings/v8/v8_member_installer.cc
+++ bridge/bindings/v8/v8_member_installer.cc
@@ -3,7 +3,7 @@
 * Copyright (C) 2022-present The WebF authors. All rights reserved.
 */
 
-#include "v8_member_installer.h"
+#include "bindings/v8/v8_member_installer.h"
 #include "core/executing_context.h"
 
 namespace webf {
