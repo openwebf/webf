a31ecca251ba15303512d0acb155a69fd5dc4f2b
LeuisKen <leuisken@gmail.com>
2024-09-02 23:33:07 +0200

    event & event_target
    - add event properties
    - add dispatch_event

--- bridge/include/plugin_api/event.h
+++ bridge/include/plugin_api/event.h
@@ -6,6 +6,7 @@
 #define WEBF_CORE_RUST_API_EVENT_H_
 
 #include "webf_value.h"
+#include "event_target.h"
 
 namespace webf {
 
@@ -14,9 +15,47 @@ typedef struct SharedExceptionState SharedExceptionState;
 typedef struct ExecutingContext ExecutingContext;
 typedef struct Event Event;
 
+using WebFEventGetBubbles = bool (*)(Event*);
+using WebFEventGetCancelable = bool (*)(Event*);
+using WebFEventGetCurrentTarget = WebFValue<EventTarget, EventTargetWebFMethods> (*)(Event*);
+using WebFEventGetDefaultPrevented = bool (*)(Event*);
+using WebFEventGetSrcElement = WebFValue<EventTarget, EventTargetWebFMethods> (*)(Event*);
+using WebFEventGetTarget = WebFValue<EventTarget, EventTargetWebFMethods> (*)(Event*);
+using WebFEventGetIsTrusted = bool (*)(Event*);
+using WebFEventGetTimeStamp = double (*)(Event*);
+using WebFEventGetType = const char* (*)(Event*);
+using WebFEventPreventDefault = void (*)(Event*, SharedExceptionState*);
+using WebFEventStopImmediatePropagation = void (*)(Event*, SharedExceptionState*);
+using WebFEventStopPropagation = void (*)(Event*, SharedExceptionState*);
+
 struct EventWebFMethods : public WebFPublicMethods {
 
+  static bool Bubbles(Event* event);
+  static bool Cancelable(Event* event);
+  static WebFValue<EventTarget, EventTargetWebFMethods> CurrentTarget(Event* event);
+  static bool DefaultPrevented(Event* event);
+  static WebFValue<EventTarget, EventTargetWebFMethods> SrcElement(Event* event);
+  static WebFValue<EventTarget, EventTargetWebFMethods> Target(Event* event);
+  static bool IsTrusted(Event* event);
+  static double TimeStamp(Event* event);
+  static const char* Type(Event* event);
+  static void PreventDefault(Event* event, SharedExceptionState* shared_exception_state);
+  static void StopImmediatePropagation(Event* event, SharedExceptionState* shared_exception_state);
+  static void StopPropagation(Event* event, SharedExceptionState* shared_exception_state);
   double version{1.0};
+
+  WebFEventGetBubbles event_get_bubbles{Bubbles};
+  WebFEventGetCancelable event_get_cancelable{Cancelable};
+  WebFEventGetCurrentTarget event_get_current_target{CurrentTarget};
+  WebFEventGetDefaultPrevented event_get_default_prevented{DefaultPrevented};
+  WebFEventGetSrcElement event_get_src_element{SrcElement};
+  WebFEventGetTarget event_get_target{Target};
+  WebFEventGetIsTrusted event_get_is_trusted{IsTrusted};
+  WebFEventGetTimeStamp event_get_time_stamp{TimeStamp};
+  WebFEventGetType event_get_type{Type};
+  WebFEventPreventDefault event_prevent_default{PreventDefault};
+  WebFEventStopImmediatePropagation event_stop_immediate_propagation{StopImmediatePropagation};
+  WebFEventStopPropagation event_stop_propagation{StopPropagation};
 };
 
 }  // namespace webf
--- bridge/rusty_webf_sys/src/event.rs
+++ bridge/rusty_webf_sys/src/event.rs
@@ -2,24 +2,122 @@
 * Copyright (C) 2022-present The WebF authors. All rights reserved.
 */
 use std::ffi::{c_char, c_double, c_void};
-use crate::OpaquePtr;
+use crate::{event_target::{EventTarget, EventTargetMethods, EventTargetRustMethods}, exception_state::ExceptionState, executing_context::ExecutingContext, OpaquePtr, RustValue};
 
 #[repr(C)]
 pub struct EventRustMethods {
   pub version: c_double,
+  pub bubbles: extern "C" fn(ptr: *const OpaquePtr) -> bool,
+  pub cancelable: extern "C" fn(ptr: *const OpaquePtr) -> bool,
+  pub current_target: extern "C" fn(ptr: *const OpaquePtr) -> RustValue<EventTargetRustMethods>,
+  pub default_prevented: extern "C" fn(ptr: *const OpaquePtr) -> bool,
+  pub src_element: extern "C" fn(ptr: *const OpaquePtr) -> RustValue<EventTargetRustMethods>,
+  pub target: extern "C" fn(ptr: *const OpaquePtr) -> RustValue<EventTargetRustMethods>,
+  pub is_trusted: extern "C" fn(ptr: *const OpaquePtr) -> bool,
+  pub time_stamp: extern "C" fn(ptr: *const OpaquePtr) -> c_double,
+  pub type_: extern "C" fn(ptr: *const OpaquePtr) -> *const c_char,
+  pub prevent_default: extern "C" fn(ptr: *const OpaquePtr, exception_state: *const OpaquePtr) -> c_void,
+  pub stop_immediate_propagation: extern "C" fn(ptr: *const OpaquePtr, exception_state: *const OpaquePtr) -> c_void,
+  pub stop_propagation: extern "C" fn(ptr: *const OpaquePtr, exception_state: *const OpaquePtr) -> c_void,
 }
 
 pub struct Event {
   pub ptr: *const OpaquePtr,
+  context: *const ExecutingContext,
   method_pointer: *const EventRustMethods,
 }
 
 impl Event {
   /// Initialize the element instance from cpp raw pointer.
-  pub fn initialize(ptr: *const OpaquePtr, method_pointer: *const EventRustMethods) -> Event {
+  pub fn initialize(ptr: *const OpaquePtr, context: *const ExecutingContext, method_pointer: *const EventRustMethods) -> Event {
     Event {
       ptr,
+      context,
       method_pointer
     }
   }
+
+  pub fn bubbles(&self) -> bool {
+    let value = unsafe {
+      ((*self.method_pointer).bubbles)(self.ptr)
+    };
+    value
+  }
+
+  pub fn cancelable(&self) -> bool {
+    let value = unsafe {
+      ((*self.method_pointer).cancelable)(self.ptr)
+    };
+    value
+  }
+
+  pub fn current_target(&self) -> EventTarget {
+    let value = unsafe {
+      ((*self.method_pointer).current_target)(self.ptr)
+    };
+    EventTarget::initialize(value.value, self.context, value.method_pointer)
+  }
+
+  pub fn default_prevented(&self) -> bool {
+    let value = unsafe {
+      ((*self.method_pointer).default_prevented)(self.ptr)
+    };
+    value
+  }
+
+  pub fn src_element(&self) -> EventTarget {
+    let value = unsafe {
+      ((*self.method_pointer).src_element)(self.ptr)
+    };
+    EventTarget::initialize(value.value, self.context, value.method_pointer)
+  }
+
+  pub fn target(&self) -> EventTarget {
+    let value = unsafe {
+      ((*self.method_pointer).target)(self.ptr)
+    };
+    EventTarget::initialize(value.value, self.context, value.method_pointer)
+  }
+
+  pub fn is_trusted(&self) -> bool {
+    let value = unsafe {
+      ((*self.method_pointer).is_trusted)(self.ptr)
+    };
+    value
+  }
+
+  pub fn time_stamp(&self) -> f64 {
+    let value = unsafe {
+      ((*self.method_pointer).time_stamp)(self.ptr)
+    };
+    value
+  }
+
+  pub fn type_(&self) -> String {
+    let value = unsafe {
+      ((*self.method_pointer).type_)(self.ptr)
+    };
+    let value = unsafe { std::ffi::CStr::from_ptr(value) };
+    let value = value.to_str().unwrap();
+    value.to_string()
+  }
+
+  pub fn prevent_default(&self, exception_state: &ExceptionState) {
+    unsafe {
+      ((*self.method_pointer).prevent_default)(self.ptr, exception_state.ptr);
+    }
+  }
+
+  pub fn stop_immediate_propagation(&self, exception_state: &ExceptionState) {
+    unsafe {
+      ((*self.method_pointer).stop_immediate_propagation)(self.ptr, exception_state.ptr);
+    }
+  }
+
+  pub fn stop_propagation(&self, exception_state: &ExceptionState) {
+    unsafe {
+      ((*self.method_pointer).stop_propagation)(self.ptr, exception_state.ptr);
+    }
+  }
+
 }
