a1c41c096f7c7b865227aa59ca0b3585c0c8d392
jwxbond <jwxbond@gmail.com>
2024-10-16 22:15:08 +0800

    feat: Path2D support copy and SVG path constructor

--- bridge/core/html/canvas/path_2d.cc
+++ bridge/core/html/canvas/path_2d.cc
@@ -12,6 +12,12 @@ Path2D* Path2D::Create(ExecutingContext* context, ExceptionState& exception_stat
   return MakeGarbageCollected<Path2D>(context, exception_state);
 }
 
+Path2D* Path2D::Create(ExecutingContext* context,
+                       const std::shared_ptr<QJSUnionPath2DDomString>& init,
+                       ExceptionState& exception_state) {
+  return MakeGarbageCollected<Path2D>(context, init, exception_state);
+}
+
 Path2D::Path2D(ExecutingContext* context, ExceptionState& exception_state)
   : BindingObject(context->ctx()) {
   NativeValue arguments[0];
@@ -20,14 +26,30 @@ Path2D::Path2D(ExecutingContext* context, ExceptionState& exception_state)
                                                               CreateBindingObjectType::kCreatePath2D, arguments, 0);
 }
 
-void Path2D::addPath(Path2D* path, DOMMatrixReadonly* dom_matrix, ExceptionState& exception_state) {
+Path2D::Path2D(ExecutingContext* context,
+               const std::shared_ptr<QJSUnionPath2DDomString>& init,
+               ExceptionState& exception_state)
+  : BindingObject(context->ctx()) {
+  NativeValue arguments[1];
+  if (init->IsDomString()) {
+    arguments[0] = NativeValueConverter<NativeTypeString>::ToNativeValue(ctx(), init->GetAsDomString());
+  } else if (init->IsPath2D()) {
+    arguments[0] = NativeValueConverter<NativeTypePointer<Path2D>>::ToNativeValue(init->GetAsPath2D());
+  }
+
+  GetExecutingContext()->dartMethodPtr()->createBindingObject(GetExecutingContext()->isDedicated(),
+                                                              GetExecutingContext()->contextId(), bindingObject(),
+                                                              CreateBindingObjectType::kCreatePath2D, arguments, 1);
+}
+
+void Path2D::addPath(Path2D* path, DOMMatrixReadonly* dom_matrix, ExceptionState& exception_state) const {
   NativeValue arguments[] = {NativeValueConverter<NativeTypePointer<Path2D>>::ToNativeValue(path),
                             NativeValueConverter<NativeTypePointer<DOMMatrixReadonly>>::ToNativeValue(dom_matrix)};
   InvokeBindingMethod(binding_call_methods::kaddPath, 2, arguments, FlushUICommandReason::kDependentsOnElement,
                       exception_state);
 }
 
-void Path2D::addPath(webf::Path2D* path, webf::ExceptionState& exception_state) {
+void Path2D::addPath(webf::Path2D* path, webf::ExceptionState& exception_state) const {
   NativeValue arguments[] = {NativeValueConverter<NativeTypePointer<Path2D>>::ToNativeValue(path)};
   InvokeBindingMethod(binding_call_methods::kaddPath, 1, arguments, FlushUICommandReason::kDependentsOnElement,
                       exception_state);
