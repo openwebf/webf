adb43859de77546c2d1b53c9d5b3f339f9dd6a6f
jwxbond <jwxbond@gmail.com>
2024-10-23 14:55:34 +0800

    feat: add test case roundRect

--- integration_tests/specs/dom/elements/canvas/context2d.ts
+++ integration_tests/specs/dom/elements/canvas/context2d.ts
@@ -219,6 +219,48 @@ describe('Canvas context 2d', () => {
     await snapshot(canvas);
   });
 
+  it('should work with roundRect', async (done) => {
+    const canvas = <canvas height="400" width="400" />;
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+
+    ctx.scale(0.7, 0.7);
+
+    // 半径为零的圆角矩形（指定为数字）
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.roundRect(10, 20, 150, 100, 0);
+    ctx.stroke();
+    
+    // 半径为 40px 的圆角矩形（单元素列表）
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.roundRect(10, 20, 150, 100, [40]);
+    ctx.stroke();
+    
+    // 具有两个不同半径的圆角矩形
+    ctx.strokeStyle = "orange";
+    ctx.beginPath();
+    ctx.roundRect(10, 150, 150, 100, [10, 40]);
+    ctx.stroke();
+    
+    
+    // 具有四个不同半径的圆角矩形
+    ctx.strokeStyle = "green";
+    ctx.beginPath();
+    ctx.roundRect(400, 20, 200, 100, [0, 30, 50, 60]);
+    ctx.stroke();
+    
+    // 向后绘制的相同矩形
+    ctx.strokeStyle = "magenta";
+    ctx.beginPath();
+    ctx.roundRect(400, 150, -200, -100, [0, 30, 50, 60]);
+    ctx.stroke();
+    
+    await snapshot(canvas);
+
+  });
+
   it('should work with transform and resetTransform', async () => {
     const canvas = <canvas />;
     document.body.appendChild(canvas);
