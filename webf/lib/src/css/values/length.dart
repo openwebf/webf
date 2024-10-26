775a88d19404a45718d1b9f224dfadb3e6c0e25b
andycall <andycall@openwebf.com>
2024-10-26 22:52:02 +0800

    fix: fix CSSLengthValue null error.

--- webf/lib/src/css/values/length.dart
+++ webf/lib/src/css/values/length.dart
@@ -66,7 +66,7 @@ class CSSLengthValue {
     }
 
     if (isViewportSizeRelatedLength()) {
-      renderStyle!.addViewportSizeRelativeProperty();
+      renderStyle?.addViewportSizeRelativeProperty();
     }
   }
 
