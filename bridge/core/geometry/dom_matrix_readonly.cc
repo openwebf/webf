22d2854234613af857a312f63a76566e3d353b05
jwxbond <jwxbond@gmail.com>
2024-10-29 22:11:15 +0800

    fix: compile error

--- bridge/core/geometry/dom_matrix_readonly.cc
+++ bridge/core/geometry/dom_matrix_readonly.cc
@@ -183,19 +183,11 @@ DOMMatrix* DOMMatrixReadonly::inverse(ExceptionState& exception_state) const {
   return MakeGarbageCollected<DOMMatrix>(GetExecutingContext(), native_binding_object);
 }
 
-DOMMatrix* DOMMatrixReadonly::multiply(const DOMMatrix* matrix,
+DOMMatrix* DOMMatrixReadonly::multiply(DOMMatrix* matrix,
                                        ExceptionState& exception_state) const {
-
-  NativeValue arguments[0] = NativeValueConverter<NativeTypePointer<DOMMatrix>>::ToNativeValue(matrix);
-
-  // if (matrix->IsDOMMatrix()) {
-  //   arguments[0] =
-  //       NativeValueConverter<NativeTypePointer<DOMMatrix>>::ToNativeValue(matrix->GetAsDOMMatrix());
-  // } else if (matrix->IsSequenceDouble()) {
-  //   arguments[0] =
-  //       NativeValueConverter<NativeTypeArray<NativeTypeDouble>>::ToNativeValue(matrix->GetAsSequenceDouble());
-  // }
-
+  NativeValue arguments[] = {
+    NativeValueConverter<NativeTypePointer<DOMMatrix>>::ToNativeValue(matrix)
+  };
   NativeValue value = InvokeBindingMethod(binding_call_methods::kmultiply, sizeof(arguments) / sizeof(NativeValue),
                                           arguments, FlushUICommandReason::kDependentsOnElement, exception_state);
   NativeBindingObject* native_binding_object =
