722eb096ed7905ad5ae6d6c9895f69ea15dfc80a
andycall <andycall@openwebf.com>
2024-11-01 02:00:19 +0800

    fix: fix css bitset test.

--- bridge/core/css/properties/css_bitset_test.cc
+++ bridge/core/css/properties/css_bitset_test.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <bitset>
 #include "gtest/gtest.h"
 #include "core/css/properties/css_bitset.h"
 
