c691c5166b8b1cbb672e352d8a7ad4d997a21292
jwxbond <jwxbond@gmail.com>
2024-11-09 14:52:22 +0800

    feat: implement formMatrix in dart

--- webf/lib/src/module/dom_matrix.dart
+++ webf/lib/src/module/dom_matrix.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:webf/bridge.dart';
+import 'package:webf/foundation.dart';
+import 'package:webf/geometry.dart';
 import 'package:webf/module.dart';
 
 class DOMMatrixModule extends BaseModule {
@@ -10,7 +14,13 @@ class DOMMatrixModule extends BaseModule {
   @override
   invoke(String method, params, InvokeModuleCallback callback) {
     if (method == 'fromMatrix') {
-        print('call DOMMatrix.fromMatrix');
+      if (params.runtimeType == DOMMatrix) {
+        DOMMatrix domMatrix = (params as DOMMatrix);
+        return DOMMatrix.fromMatrix4(
+            BindingContext(domMatrix.ownerView, domMatrix.ownerView.contextId, allocateNewBindingObject()),
+            domMatrix.matrix.clone(),
+            domMatrix.is2D);
+      }
     }
   }
 
