c5dfc3507dd0d77fdb83b245a44227a3384dd451
andycall <andycall@openwebf.com>
2024-11-10 23:40:22 +0800

    fix: fix DOMMatrix.fromMatrix.

--- bridge/core/geometry/dom_matrix.d.ts
+++ bridge/core/geometry/dom_matrix.d.ts
@@ -2,5 +2,6 @@ import {DOMMatrixInit} from "./dom_matrix_init";
 import {DOMMatrixReadOnly} from "./dom_matrix_read_only";
 
 interface DOMMatrix extends DOMMatrixReadOnly {
+    fromMatrix(matrix: DOMMatrix): StaticMethod<DOMMatrix>;
     new(init?: number[] | DOMMatrixInit): DOMMatrix;
 }
