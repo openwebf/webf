880d16db4ebe86c7bf9cf82577ecc4f3136b81d2
andycall <andycall@openwebf.com>
2024-11-10 23:42:34 +0800

    fix: fix DOMPoint.fromPoint.

--- bridge/core/geometry/dom_point.d.ts
+++ bridge/core/geometry/dom_point.d.ts
@@ -2,5 +2,6 @@ import {DOMPointInit} from "./dom_point_init";
 import {DOMPointReadOnly} from "./dom_point_read_only";
 
 interface DOMPoint extends DOMPointReadOnly {
+    fromPoint(point: DOMPoint): StaticMethod<DOMPoint>;
     new(x?: number | DOMPointInit, y?: number, z?: number, w?: number): DOMPoint;
 }
