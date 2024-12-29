fa777356ec68bbb5a6570192e4c207c735ad34bf
Jiaxun Wei <leuisken@gmail.com>
2024-12-29 14:47:14 +0800

    feat: add u8 bytes native value support

--- bridge/foundation/native_value.h
+++ bridge/foundation/native_value.h
@@ -55,6 +55,7 @@ NativeValue Native_NewInt64(int64_t value);
 NativeValue Native_NewList(uint32_t argc, NativeValue* argv);
 NativeValue Native_NewPtr(JSPointerType pointerType, void* ptr);
 NativeValue Native_NewJSON(JSContext* ctx, const ScriptValue& value, ExceptionState& exception_state);
+NativeValue Native_NewUint8Bytes(uint32_t length, uint8_t* bytes);
 
 JSPointerType GetPointerTypeOfNativePointer(NativeValue native_value);
 
