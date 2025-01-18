27df4f2975d7125e32332f2a5bc0a04b04aa5aea
LeuisKen <leuisken@gmail.com>
2025-01-18 20:36:11 +0800

    feat: naive test runner

--- integration_tests/rust_builder/rust/src/lib.rs
+++ integration_tests/rust_builder/rust/src/lib.rs
@@ -1,35 +1,41 @@
 use std::ffi::c_void;
 use webf_sys::executing_context::ExecutingContextRustMethods;
-use webf_sys::{initialize_webf_api, RustValue};
+use webf_sys::{initialize_webf_api, ExecutingContext, RustValue};
+
+fn test_user_agent(context: ExecutingContext) {
+  let navigator = context.navigator();
+  let exception_state = context.create_exception_state();
+  let ua_string = navigator.user_agent(&exception_state);
+
+  assert!(ua_string.contains("WebF"));
+}
+
+fn test_hardware_concurrency(context: ExecutingContext) {
+  let navigator = context.navigator();
+  let exception_state = context.create_exception_state();
+  let hardware_concurrency = navigator.hardware_concurrency(&exception_state);
+
+  assert!(hardware_concurrency > 0);
+}
+
+fn webf_test_runner(tests: &[&dyn Fn(ExecutingContext)], context: ExecutingContext) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test(context.clone());
+    println!("Test passed");
+  }
+}
 
 #[no_mangle]
 pub extern "C" fn init_webf_test_app(handle: RustValue<ExecutingContextRustMethods>) -> *mut c_void {
   let context = initialize_webf_api(handle);
-  println!("Context created");
-  let exception_state = context.create_exception_state();
-  let navigator = context.navigator();
 
-  let ua_string = navigator.user_agent(&exception_state);
-  println!("User Agent: {}", ua_string);
+  let tests: &[&dyn Fn(ExecutingContext)] = &[
+    &test_user_agent,
+    &test_hardware_concurrency,
+  ];
 
-  let local_storage = context.local_storage();
+  webf_test_runner(tests, context);
 
-  let result = local_storage.set_item("test", "test2", &exception_state);
-
-  match result {
-    Ok(_) => {
-      println!("Local Storage Set Item Success");
-    },
-    Err(err) => {
-      println!("Local Storage Set Item Failed: {:?}", err);
-    }
-  }
-
-  println!("Local Storage value for \"a\": {:?}", local_storage.get_item("a", &exception_state));
-  println!("Local Storage Keys: {:?}", local_storage.get_all_keys(&exception_state));
-  println!("Local Storage Length: {:?}", local_storage.length(&exception_state));
-  println!("Local Storage value for \"test\": {:?}", local_storage.get_item("test", &exception_state));
-
-  local_storage.clear(&exception_state);
   std::ptr::null_mut()
 }
