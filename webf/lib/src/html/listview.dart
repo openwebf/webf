c3598b934602364d174e063ac7e9cfb7f991eecc
andycall <andycall@openwebf.com>
2025-02-23 02:27:18 +0800

    feat: add scrollTop and scrollTo DOM API support for ListView Element.

--- webf/lib/src/html/listview.dart
+++ webf/lib/src/html/listview.dart
@@ -8,22 +8,39 @@ const WEBF_LISTVIEW = 'WEBF-LISTVIEW';
 
 class FlutterListViewElement extends WidgetElement {
   FlutterListViewElement(BindingContext? context) : super(context);
+
+  Axis scrollDirection = Axis.vertical;
+
+  @override
+  void scroll(double x, double y, [bool withAnimation = false]) {
+    ScrollController? scrollController = PrimaryScrollController.maybeOf(context);
+    scrollController?.position.moveTo(
+      scrollDirection == Axis.vertical ? y : x,
+      duration: null,
+      curve: null,
+    );
+  }
+
+  @override
+  set scrollTop(double value) {
+    scroll(0, value);
+  }
+
   @override
   Widget build(BuildContext context, ChildNodeList childNodes) {
     return WebFChildNodeSize(
         ownerElement: this,
         child: ListView.builder(
+          scrollDirection: scrollDirection,
           itemCount: childNodes.length,
           itemBuilder: (context, index) {
             Node? node = childNodes.elementAt(index);
             if (node is dom.Element) {
-              return LayoutBoxWrapper(
-                  ownerElement: node,
-                  child: childNodes.elementAt(index).toWidget()
-              );
+              return LayoutBoxWrapper(ownerElement: node, child: childNodes.elementAt(index).toWidget());
             }
             return node.toWidget();
           },
+          primary: true,
           padding: const EdgeInsets.all(0),
           // controller: controller,
           physics: const AlwaysScrollableScrollPhysics(),
