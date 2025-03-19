cb2325eff70086d413254f0f19f3c9ae511ced5c
ChrisCindy <yxy0919@foxmail.com>
2025-03-19 23:31:50 +0800

    feat: support render markdown

--- webf/example/news_miracleplus/src/Components/post/PostContent.vue
+++ webf/example/news_miracleplus/src/Components/post/PostContent.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="post-content">
       <div class="title">{{ post.title }}</div>
-      <div class="content" v-html="formattedContent"></div>
+      <div class="content" v-if="isRichContent">
+        <div class="content" v-html="formattedContent"></div>
+      </div>
+      <div class="markdown-wrapper" v-else>
+        <div class="markdown-content" v-html="renderedMarkdown"></div>
+      </div>
     </div>
   </template>
   
   <script>
+  import markdownit from 'markdown-it'
   export default {
     name: 'PostContent',
     props: {
@@ -17,10 +23,26 @@
     computed: {
       formattedContent() {
         return this.formatContent(this.post.content);
+      },
+      isRichContent() {
+        let result = false;
+        try {
+          JSON.parse(this.post.content);
+          result = true;
+        } catch (e) {
+          result = false;
+        }
+        return result;
+      },
+      renderedMarkdown() {
+        const md = markdownit()
+        const result = md.render(this.post.content || '');
+        return result;
       }
     },
     methods: {
       formatContent(content) {
+        console.log('content', content);
         try {
           const parsedContent = JSON.parse(content);
           return parsedContent.map(block => {
