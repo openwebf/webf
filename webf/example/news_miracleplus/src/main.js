3fe3e9095c6a906191304d51795bce0e83623a5d
andycall <andycall@openwebf.com>
2025-06-24 22:04:39 +0800

    chore: move miracle plus news to standalone project.

--- webf/example/news_miracleplus/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import './main.css';
-
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia).mount('#app')
--- webf/example/news_miracleplus/src/utils/formatAvatar.js
+++
@@ -1,9 +0,0 @@
-export default function formatAvatar(avatar) {
-  if (!avatar) {
-    avatar = '/img/avatar/defaultavatar4.png';
-  }
-  if (avatar.startsWith('http')) {
-    return avatar;
-  }
-  return `https://news.miracleplus.com${avatar}`;
-}
