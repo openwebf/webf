41e232aa6c069dea811e7734acad03f285a51a0d
andycall <andycall@openwebf.com>
2025-07-03 07:29:35 +0800

    refactor: use new react libs for react project.

--- webf_apps/react_project/src/App.tsx
+++ webf_apps/react_project/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { WebFRouterLink } from './CustomElements/RouterLink';
+import { Routes, Route } from '@openwebf/react-router';
 import LandingPage from './Pages/LandingPage';
 import ArrayBufferDemo from './Pages/ArrayBufferDemo';
 import ModalPopupDemo from './Pages/ModalPopupDemo';
@@ -9,23 +9,26 @@ function App() {
   let [inputValue, updateValue] = useState('');
   return (
     <div className="App">
-      <WebFRouterLink path="/">
-        <LandingPage />
-        <input
-        type="checkbox"
-          value={inputValue}
-          onChange={() => console.log('checked', inputValue)}
-        />
-      </WebFRouterLink>
-      <WebFRouterLink path="/array-buffer-demo">
-        <ArrayBufferDemo />
-      </WebFRouterLink>
-      <WebFRouterLink path="/modal-popup-demo">
-        <ModalPopupDemo />
-      </WebFRouterLink>
-      <WebFRouterLink path="/flutter">
-
-      </WebFRouterLink>
+      <Routes>
+        <Route path="/" element={<div>
+          <div
+          id="target"
+            style={{
+              borderBottom: '1px dashed red'
+            }}
+          >
+            test
+          </div>
+          {/* <LandingPage />
+          <input
+            type="checkbox"
+            value={inputValue}
+            onChange={() => console.log('checked', inputValue)}
+          /> */}
+        </div>}></Route>
+        {/* <Route path="/array-buffer-demo" element={<ArrayBufferDemo />}></Route>
+        <Route path="/modal-popup-demo" element={<ModalPopupDemo />} /> */}
+      </Routes>
     </div>
   );
 }
--- webf_apps/react_project/src/CustomElements/FlutterCupertinoModalPopup.tsx
+++
@@ -1,4 +0,0 @@
-import { FlutterCupertinoModalPopup } from "@openwebf/cupertino-react-components";
-export type { FlutterCupertinoModalPopupElement } from "@openwebf/cupertino-react-components";
-
-export default FlutterCupertinoModalPopup;
