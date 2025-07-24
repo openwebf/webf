6316ef85967d7b2599b625a1f6bee05a6ae1721f
andycall <andycall@openwebf.com>
2025-07-24 17:33:17 +0800

    feat: tempoary disable android 16 page size support.

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -756,7 +756,7 @@ task('build-android-webf-lib', (done) => {
     } else if (platform == 'linux') {
       androidHome = path.join(process.env.HOME, 'Android/Sdk');
     }
-    const ndkVersion = '28.2.13676358';
+    const ndkVersion = '22.1.7171670';
     ndkDir = path.join(androidHome, 'ndk', ndkVersion);
 
     if (!fs.existsSync(ndkDir)) {
