8c68015eca6632c60633c95d06cc22884d728045
andycall <andycall@openwebf.com>
2025-08-12 18:04:48 +0800

    feat: upgrade ndk version

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -764,7 +764,7 @@ task('build-android-webf-lib', (done) => {
     } else if (platform == 'linux') {
       androidHome = path.join(process.env.HOME, 'Android/Sdk');
     }
-    const ndkVersion = '22.1.7171670';
+    const ndkVersion = '27.3.13750724';
     ndkDir = path.join(androidHome, 'ndk', ndkVersion);
 
     if (!fs.existsSync(ndkDir)) {
