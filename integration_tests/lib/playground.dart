1f2e15765401e495e1a481d5ae0e5636a8ac48ae
andycall <andycall@openwebf.com>
2025-08-21 14:20:43 -0700

    test: add playground for integration test

---
+++ integration_tests/lib/playground.dart
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022-present The WebF authors. All rights reserved.
+ */
+import 'dart:async';
+import 'dart:io';
+
+import 'package:ansicolor/ansicolor.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:webf/css.dart';
+import 'package:webf/webf.dart';
+import 'package:webf/devtools.dart';
+
+import 'custom_elements/main.dart';
+import 'test_module.dart';
+import 'local_http_server.dart';
+import 'utils/mem_leak_detector.dart';
+import 'webf_tester.dart';
+import 'modules/array_buffer_module.dart';
+
+// By CLI: `KRAKEN_ENABLE_TEST=true flutter run`
+void main() async {
+  // Initialize the controller manager
+  WebFControllerManager.instance.initialize(WebFControllerManagerConfig(
+      maxAliveInstances: 1,
+      useDioForNetwork: false,
+      maxAttachedInstances: 1,
+      onControllerDisposed: (String name, WebFController controller) {
+        print('controller disposed: $name $controller');
+      }));
+
+  defineWebFCustomElements();
+
+  // Set render font family AlibabaPuHuiTi to resolve rendering difference.
+  CSSText.DEFAULT_FONT_FAMILY_FALLBACK = ['AlibabaPuHuiTi'];
+
+  runZonedGuarded(() {
+    runApp(MaterialApp(
+      title: 'webF Integration Tests',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('WebF Integration Tests')),
+        body: Stack(
+          children: [
+            WebF.fromControllerName(
+                controllerName: 'test', bundle: WebFBundle.fromUrl('http://127.0.0.1:3300/kraken_debug_server.js')),
+            WebFInspectorFloatingPanel(),
+          ],
+        ),
+      ),
+    ));
+  }, (Object error, StackTrace stack) {
+    print('$error\n$stack');
+  });
+}
