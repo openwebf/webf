7aa31e6b41845c8a7d62cc22f261a32c9def2770
ChrisCindy <yxy0919@foxmail.com>
2025-08-29 22:53:28 +0800

    refactor(WebFPlugin): remove deprecated registerWith method for cleaner code

--- webf/android/src/main/java/com/openwebf/webf/WebFPlugin.java
+++ webf/android/src/main/java/com/openwebf/webf/WebFPlugin.java
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * WebFPlugin
@@ -23,21 +22,7 @@ public class WebFPlugin implements FlutterPlugin, MethodCallHandler {
   private Context mContext;
   private WebF mWebF;
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "webf");
-    WebFPlugin plugin = new WebFPlugin();
-    plugin.mContext = registrar.context();
-    channel.setMethodCallHandler(plugin);
-  }
+
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
