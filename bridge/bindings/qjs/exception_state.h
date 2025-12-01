a0998d1f773cae8b4e64e4fc9e28ef7004282712
andycall <andycall@openwebf.com>
2025-12-01 13:22:01 -0800

    doc: update license header for the bridge/bindings

--- bridge/bindings/qjs/exception_state.h
+++ bridge/bindings/qjs/exception_state.h
@@ -1,6 +1,10 @@
 /*
+ * Copyright (C) 2024-present The OpenWebF Company. All rights reserved.
+ * Licensed under GNU GPL with Enterprise exception.
+ */
+/*
  * Copyright (C) 2019-2022 The Kraken authors. All rights reserved.
- * Copyright (C) 2022-present The WebF authors. All rights reserved.
+ * Copyright (C) 2022-2024 The WebF authors. All rights reserved.
  */
 #ifndef BRIDGE_EXCEPTION_STATE_H
 #define BRIDGE_EXCEPTION_STATE_H
