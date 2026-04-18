b601be918649dd5f743ac8ac5686631134e5c642
andycall <andycall@openwebf.com>
2026-04-18 07:34:22 -0700

    style(test): align waterfall phase filter test header with repo convention
    Use "The WebF authors" copyright text matching other test files in webf/test/
    rather than the OpenWebF Company / Enterprise exception text the plan had.

--- webf/test/src/devtools/waterfall_phase_filter_test.dart
+++ webf/test/src/devtools/waterfall_phase_filter_test.dart
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2024-present The OpenWebF Company. All rights reserved.
- * Licensed under GNU GPL with Enterprise exception.
+ * Copyright (C) 2026-present The WebF authors. All rights reserved.
  */
 
 import 'package:flutter/material.dart';
