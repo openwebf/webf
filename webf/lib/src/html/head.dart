0c1bff42859be4a75295f8936aa88ac049a416bc
andycall <andycall@openwebf.com>
2026-04-19 03:13:42 -0700

    style(html): wrap fetchCSS beginEntry call to fit 120-char limit

--- webf/lib/src/html/head.dart
+++ webf/lib/src/html/head.dart
@@ -269,7 +269,8 @@ class LinkElement extends Element {
         // Increment count when request.
         ownerDocument.incrementRequestCount();
 
-        final fetchEntry = PerformanceTracker.instance.beginEntry(kSubTypeNetworkResponse, url, metadata: {'kind': 'css'});
+        final fetchEntry = PerformanceTracker.instance
+            .beginEntry(kSubTypeNetworkResponse, url, metadata: {'kind': 'css'});
         await bundle.resolve(baseUrl: ownerDocument.controller.url, uriParser: ownerDocument.controller.uriParser);
         await bundle.obtainData(ownerView.contextId);
         fetchEntry?.end();
